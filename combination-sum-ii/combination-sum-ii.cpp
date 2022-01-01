6cf9acfb97b9e0d47e21326ff41d196387305369
mayank <46345959+mayank171@users.noreply.github.com>
2022-01-01 17:30:08 +0530

    Time: 69 ms (7.41%), Space: 16.2 MB (15.91%) - LeetHub

---
+++ combination-sum-ii/combination-sum-ii.cpp
@@ -0,0 +1,89 @@
+class Solution {
+    
+private:
+    
+    void f1(vector<int> &candidates,int ind,int &sum,int target,vector<vector<int>> &ans,vector<int> temp,int n)
+    {
+        if(ind==n)
+        {
+            if(sum==target)
+            {
+                
+                ans.push_back(temp);
+                
+            }
+            return ;
+        }
+        
+         if(sum==target)
+            {
+                
+                ans.push_back(temp);
+                return ;
+                
+            }
+        
+        if(sum>target)
+        {
+            return ;
+        }
+        
+        int flag=0;
+        for(int i=ind;i<n;i++)
+        {
+//              if(i>ind && candidates[i]==candidates[i-1])
+//                 continue;
+            
+               
+             if(flag==0)
+             {
+                sum+=candidates[i];
+                temp.push_back(candidates[i]);
+                f1(candidates,i+1,sum,target,ans,temp,n);
+                temp.pop_back();
+                sum-=candidates[i];
+                flag=1;  
+             }
+             
+            if(i+1<n && candidates[i]==candidates[i+1])
+            {
+                continue;
+            }
+            else
+            {
+                flag=0;
+            }
+                 
+            
+       
+        }
+        
+        
+    }
+    
+    
+public:
+    
+    
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        
+        sort(candidates.begin(),candidates.end());
+        
+        int ind=0;
+        
+        int sum=0;
+        
+        int n=candidates.size();
+        
+        vector<vector<int>> ans;
+        vector<int> temp;
+        
+       // unordered_set<int> s;
+        
+        
+        f1(candidates,ind,sum,target,ans,temp,n);
+        
+        return ans;
+        
+    }
+};
