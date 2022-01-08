7dc3d5ba96dfaaccf11cf1f2f981521daeab1050
mayank <46345959+mayank171@users.noreply.github.com>
2022-01-08 09:11:10 +0530

    Time: 18 ms (10.00%), Space: 9.5 MB (54.20%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.cpp
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -2,22 +2,58 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         
-        unordered_map<int,int> m;
+//         unordered_map<int,int> m;
         
-        int N=nums.size();
+//         int N=nums.size();
         
-        for(int i=0;i<N;i++)
+//         for(int i=0;i<N;i++)
+//         {
+//             m[nums[i]]++;
+//         }
+        
+//         for(auto it:m)
+//         {
+//             if(it.second==1)
+//             {
+//                 return it.first;
+//             }
+//         }
+//         return 0;
+        
+        int n=nums.size();
+        
+        if(n==1)
         {
-            m[nums[i]]++;
+            return nums[0];
         }
         
-        for(auto it:m)
+        sort(nums.begin(),nums.end());
+        
+        int ans=0;
+        int flag=0;
+        
+        for(int i=1;i<n-1;i+=3)
         {
-            if(it.second==1)
+            if(nums[i-1]!=nums[i] && nums[i]==nums[i+1])
             {
-                return it.first;
+                flag=1;
+                ans=nums[i-1];
+                break;
             }
+            else if(nums[i-1]==nums[i] && nums[i]!=nums[i+1])
+            {
+                flag=1;
+                ans=nums[i+1];
+                break;
+            }
+            
         }
-        return 0;
+        
+        if(flag==0)
+        {
+            ans=nums[n-1];
+        }
+        
+        return ans;
     }
 };
