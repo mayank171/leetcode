84eac0c906a35f9a74a1c5fbe6914c12964b345c
mayank <46345959+mayank171@users.noreply.github.com>
2022-01-25 16:26:29 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (19.73%) - LeetHub

---
+++ 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        
+        int l=s.length();
+        
+        stack<char> stk;
+        
+        for(int i=0;i<l;i++)
+        {
+            if(s[i]=='(')
+            {
+                // if(!stk.empty() && stk.top()==')')
+                // {
+                //     stk.pop();
+                // }
+                // else 
+                // {
+                    stk.push('(');
+               // }
+            }
+            else
+            {
+                if(!stk.empty() && stk.top()=='(')
+                {
+                    stk.pop();
+                }
+                else
+                {
+                    stk.push(')');
+                }
+            }
+        }
+        
+        return stk.size();
+        
+    }
+};
