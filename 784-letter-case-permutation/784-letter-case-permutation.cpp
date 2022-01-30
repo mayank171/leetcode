bd889bd8859366a3098b4ce2b089bd725d50f7c4
mayank <46345959+mayank171@users.noreply.github.com>
2022-01-30 16:15:34 +0530

    Time: 3 ms (95.13%), Space: 10.4 MB (69.12%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.cpp
+++ 784-letter-case-permutation/784-letter-case-permutation.cpp
@@ -9,30 +9,54 @@ private:
         {
             return ;
         }
-      
         
-        for(int i=ind;i<l;i++)
+        
+        if(s[ind]>='a' && s[ind]<='z')
         {
-            if(s[i]>='a' && s[i]<='z')
-            {
-                s[i] = s[i] & ~x;
-                cout<<s<<endl;
-                check(s,i+1,l,ans);
-                ans.push_back(s);
-                s[i] = s[i] | x;
-                
-            }
-            else if(s[i]>='A' && s[i]<='Z')
-            {
-                s[i] = s[i] | x;
-                cout<<s<<endl;
-                check(s,i+1,l,ans);
-                ans.push_back(s);
-                s[i] = s[i] & ~x;
-            }
-           
-           
+            s[ind] = s[ind] & ~x;
+            //cout<<s<<endl;
+            check(s,ind+1,l,ans);
+            ans.push_back(s);
+            s[ind] = s[ind] | x;
+            check(s,ind+1,l,ans);
+            
         }
+        else if(s[ind]>='A' && s[ind]<='Z')
+        {
+            s[ind] = s[ind] | x;
+            //cout<<s<<endl;
+            check(s,ind+1,l,ans);
+            ans.push_back(s);
+            s[ind] = s[ind] & ~x;
+            check(s,ind+1,l,ans);
+        }
+        else
+        {
+            check(s,ind+1,l,ans);
+        }
+        
+//         for(int i=ind;i<l;i++)
+//         {
+//             if(s[i]>='a' && s[i]<='z')
+//             {
+//                 s[i] = s[i] & ~x;
+//                 cout<<s<<endl;
+//                 check(s,i+1,l,ans);
+//                 ans.push_back(s);
+//                 s[i] = s[i] | x;
+                
+//             }
+//             else if(s[i]>='A' && s[i]<='Z')
+//             {
+//                 s[i] = s[i] | x;
+//                 cout<<s<<endl;
+//                 check(s,i+1,l,ans);
+//                 ans.push_back(s);
+//                 s[i] = s[i] & ~x;
+//             }
+           
+           
+//         }
     
     }
 public:
