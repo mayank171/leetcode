2b3834f745eb69ab21a5e397d42ea3755a1fe606
mayank <46345959+mayank171@users.noreply.github.com>
2022-02-04 16:17:11 +0530

    Time: 8 ms (87.52%), Space: 10.1 MB (40.98%) - LeetHub

--- 260-single-number-iii/260-single-number-iii.cpp
+++ 260-single-number-iii/260-single-number-iii.cpp
@@ -2,38 +2,79 @@ class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
         
-        sort(nums.begin(),nums.end());
         
-        int l=nums.size();
+        int x=0;
+       for(auto &it:nums)
+       {
+           x^=it;
+       }
         
-        vector<int> ans;
-        
-        for(int i=0;i<l;i++)
+        int ind=0;
+        for(int i=0;i<=31;i++)
         {
-            
-            if(i==l-1)
+            if(x&(1<<i))
             {
-                ans.push_back(nums[i]);
+                ind=i;
                 break;
             }
-            
-            if(nums[i]==nums[i+1])
-            {
-                i++;
-            }
-            else 
-            {
-                ans.push_back(nums[i]);
-            }
-            
-            if(ans.size()==2)
-            {
-                break;
-            }
-            
         }
         
+        cout<<ind<<endl;
+        
+        int xor_a=0;
+        int xor_b=0;
+        
+        for(auto &it:nums)
+        {
+             if(it&(1<<ind))
+                 xor_a^=it;
+             else
+                 xor_b^=it;
+        }
+        
+        cout<<xor_a<<" "<<xor_b<<endl;
+        
+        vector<int> ans;
+        ans.push_back(xor_a);
+        ans.push_back(xor_b);
+        
+        
         return ans;
+        
+        
+        
+//         sort(nums.begin(),nums.end());
+        
+//         int l=nums.size();
+        
+//         vector<int> ans;
+        
+//         for(int i=0;i<l;i++)
+//         {
+            
+//             if(i==l-1)
+//             {
+//                 ans.push_back(nums[i]);
+//                 break;
+//             }
+            
+//             if(nums[i]==nums[i+1])
+//             {
+//                 i++;
+//             }
+//             else 
+//             {
+//                 ans.push_back(nums[i]);
+//             }
+            
+//             if(ans.size()==2)
+//             {
+//                 break;
+//             }
+            
+//         }
+        
+//         return ans;
         
     }
 };
