7953fddc2027a10bf22db9641a7a5f5bf85a1942
mayank <46345959+mayank171@users.noreply.github.com>
2022-02-25 16:56:59 +0530

    Time: 16 ms (93.70%), Space: 16.8 MB (65.04%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
@@ -17,42 +17,96 @@ public:
 */
 
 class Solution {
+private:
+    void check(Node* root)
+    {
+        if(root==NULL)
+            return ;
+        
+        if(root->left)
+           {
+               if(root->right!=NULL)
+               {
+                   root->left->next=root->right;
+               }
+               else
+               {
+                   root->left->next=NULL;
+               }
+           }
+        if(root->right)
+           {
+               if(root->next!=NULL && root->next->left!=NULL)
+               {
+                   root->right->next=root->next->left;
+               }
+               else
+               {
+                   root->right->next=NULL;
+               }
+           }
+        
+        check(root->left);
+        check(root->right);
+            
+    }
 public:
     Node* connect(Node* root) {
         
         if(root==NULL)
             return root;
         
-        queue<Node*> q;
-        
-        q.push(root);
-        
-        while(!q.empty())
-        {
-            int size=q.size();
-            
-            for(int i=0;i<size;i++)
-            {
-                Node* node=q.front();
-                q.pop();
-                if(i+1==size)
-                {
-                    node->next=NULL;
-                }
-                else
-                {
-                    node->next=q.front();
-                }
-                
-                if(node->left)
-                    q.push(node->left);
-                if(node->right)
-                    q.push(node->right);
-                
-            }
-        }
+        root->next=NULL;
+        check(root);
         
         return root;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         queue<Node*> q;
+        
+//         q.push(root);
+        
+//         while(!q.empty())
+//         {
+//             int size=q.size();
+            
+//             for(int i=0;i<size;i++)
+//             {
+//                 Node* node=q.front();
+//                 q.pop();
+//                 if(i+1==size)
+//                 {
+//                     node->next=NULL;
+//                 }
+//                 else
+//                 {
+//                     node->next=q.front();
+//                 }
+                
+//                 if(node->left)
+//                     q.push(node->left);
+//                 if(node->right)
+//                     q.push(node->right);
+                
+//             }
+//         }
+        
+//         return root;
         
     }
 };
