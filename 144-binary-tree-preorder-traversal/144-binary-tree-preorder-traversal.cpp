12b608dee62aa8d1287058da660402036819b7c4
mayank <46345959+mayank171@users.noreply.github.com>
2022-03-02 15:38:52 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (44.58%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
@@ -13,28 +13,64 @@ class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
         
-        vector<int> ans;
+        vector<int> preorder;
         
-        if(root==NULL)
-            return ans;
+        TreeNode* cur=root;
         
-        stack<TreeNode*> stk;
-        
-        stk.push(root);
-        
-        while(!stk.empty())
+        while(cur!=NULL)
         {
-            TreeNode* node=stk.top();
-            stk.pop();
-            
-            ans.push_back(node->val);
-            if(node->right!=NULL)
-               stk.push(node->right);
-            if(node->left!=NULL)            
-            stk.push(node->left);
+            if(cur->left==NULL)
+            {
+                preorder.push_back(cur->val);
+                cur=cur->right;
+            }
+            else
+            {
+                TreeNode*prev=cur->left;
+                while(prev->right && prev->right!=cur)
+                {
+                    prev=prev->right;
+                }
+                
+                if(prev->right==NULL)
+                {
+                    prev->right=cur;
+                    preorder.push_back(cur->val);
+                    cur=cur->left;
+                }
+                else
+                {
+                    prev->right=NULL;
+                    cur=cur->right;
+                }
+            }
         }
         
-        return ans;
+        return preorder;
+        
+        
+//         vector<int> ans;
+        
+//         if(root==NULL)
+//             return ans;
+        
+//         stack<TreeNode*> stk;
+        
+//         stk.push(root);
+        
+//         while(!stk.empty())
+//         {
+//             TreeNode* node=stk.top();
+//             stk.pop();
+            
+//             ans.push_back(node->val);
+//             if(node->right!=NULL)
+//                stk.push(node->right);
+//             if(node->left!=NULL)            
+//             stk.push(node->left);
+//         }
+        
+//         return ans;
         
     }
 };
