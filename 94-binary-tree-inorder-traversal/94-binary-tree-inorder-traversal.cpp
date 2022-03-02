bce3fb2f524485e834a3a60f7ff97908bd8f1d5d
mayank <46345959+mayank171@users.noreply.github.com>
2022-03-02 15:35:01 +0530

    Time: 9 ms (8.09%), Space: 8.4 MB (76.29%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -28,50 +28,85 @@ class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
         
+        vector<int> inorder;
+        
+        TreeNode* cur=root;
+        
+        while(cur!=NULL)
+        {
+            if(cur->left==NULL)
+            {
+                inorder.push_back(cur->val);
+                cur=cur->right;
+            }
+            else
+            {
+                TreeNode* prev=cur->left;
+                while(prev->right && prev->right!=cur)
+                {
+                    prev=prev->right;
+                }
+                
+                if(prev->right==NULL)
+                {
+                    prev->right=cur;
+                    cur=cur->left;
+                }
+                else
+                {
+                    prev->right=NULL;
+                    inorder.push_back(cur->val);
+                    cur=cur->right;
+                }
+            }
+        }
+        
+        return inorder;
+        
         
 //        vector<int> nodes;
 //        inorder(root,nodes);
         
 //         return nodes;
         
-        vector<int> ans;
+//         vector<int> ans;
         
-        if(root==NULL)
-            return ans;
+//         if(root==NULL)
+//             return ans;
         
-        stack<TreeNode*> stk;
+//         stack<TreeNode*> stk;
         
-        stk.push(root);
+//         stk.push(root);
         
-        int k=0;
+//         int k=0;
         
-        while(!stk.empty())
-        {
-            TreeNode* node=stk.top();
+//         while(!stk.empty())
+//         {
+//             TreeNode* node=stk.top();
             
-            while(node->left!=NULL && k==0)
-            {
-                node=node->left;
-                stk.push(node);
-            }
+//             while(node->left!=NULL && k==0)
+//             {
+//                 node=node->left;
+//                 stk.push(node);
+//             }
             
-            TreeNode* node1=stk.top();
-            stk.pop();
+//             TreeNode* node1=stk.top();
+//             stk.pop();
             
-            ans.push_back(node1->val);
+//             ans.push_back(node1->val);
             
-            if(node1->right!=NULL)
-            {
-                k=0;
-                stk.push(node1->right);
-            }
-            else
-            {
-                k=-10001;       //pointer to remind don't go to the upper while loop
-            }
-        }
+//             if(node1->right!=NULL)
+//             {
+//                 k=0;
+//                 stk.push(node1->right);
+//             }
+//             else
+//             {
+//                 k=-10001;       //pointer to remind don't go to the upper while loop
+//             }
+//         }
         
-        return ans;
+//         return ans;
        
         
     }
