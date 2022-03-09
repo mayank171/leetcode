5b549f1b2d5388bded673451bb0be62ba223105f
mayank <46345959+mayank171@users.noreply.github.com>
2022-03-09 18:54:42 +0530

    Time: 40 ms (76.00%), Space: 10.8 MB (43.66%) - LeetHub

---
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.cpp
@@ -0,0 +1,114 @@
+class Solution {
+private:
+    
+    bool check(vector<int> &nums,int size,vector<vector<int>> &adj,int ssf,int i,int adj_size,int sum_all)
+    {
+        if(i==size)
+        {
+            for(int j=0;j<adj_size;j++)
+            {
+                if(adj[j].size()==0)
+                    return false;
+            }
+            
+            // for(int j=0;j<adj_size;j++)
+            // {
+            //     for(int k=0;k<adj[j].size();k++)
+            //     {
+            //         cout<<adj[j][k]<<" ";
+            //     }
+            //     cout<<endl;
+            // }
+            // cout<<endl;
+            
+            int sum=0;
+            for(int j=0;j<adj[0].size();j++)
+            {
+                sum+=adj[0][j];
+            }
+            
+            for(int j=1;j<adj_size;j++)
+            {
+                int sum1=0;
+                for(int k=0;k<adj[j].size();k++)
+                {
+                    sum1+=adj[j][k];
+                }
+                
+                if(sum1!=sum)
+                {
+                    return false;
+                }
+                else
+                {
+                   // cout<<sum1<<endl;
+                }
+            }
+            
+            return true;
+        }
+        
+        for(int j=1;j<adj_size;j++)
+        {
+            int sum1=0;
+            for(int k=0;k<adj[j].size();k++)
+            {
+                sum1+=adj[j][k];
+            }
+            if(sum1>sum_all)
+                return false;
+        }
+        
+        for(int j=0;j<adj_size;j++)
+        {
+            int sum1=accumulate(adj[j].begin(),adj[j].end(),0);
+            // cout<<sum1<<" ";
+            // cout<<sum_all/size<<endl;
+            if(sum1>sum_all)
+                return false;
+            
+            if(adj[j].size()>0)
+            {
+                
+                 adj[j].push_back(nums[i]);
+                // int sum1=accumulate(adj[j].begin(),adj[j].end(),0);
+                // cout<<sum1<<" "<<sum_all<<endl;
+                // if(sum1>sum_all)
+                // {
+                //     adj[j].pop_back();
+                //     continue;
+                // }
+                
+                if(check(nums,size,adj,ssf,i+1,adj_size,sum_all))
+                    return true;
+                adj[j].pop_back();
+            }
+            else
+            {
+                adj[j].push_back(nums[i]);
+                if(check(nums,size,adj,ssf+1,i+1,adj_size,sum_all))
+                    return true;
+                adj[j].pop_back();
+                break;
+            }
+        }
+        
+        return false;
+    }
+public:
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        int size=nums.size();
+        
+        sort(nums.rbegin(),nums.rend());
+        
+        vector<vector<int>> adj(k);
+        
+        int sum_all=accumulate(nums.begin(),nums.end(),0);
+        sum_all/=k;
+        
+        if(check(nums,size,adj,0,0,k,sum_all))
+            return true;
+        
+        return false;
+    }
+};
