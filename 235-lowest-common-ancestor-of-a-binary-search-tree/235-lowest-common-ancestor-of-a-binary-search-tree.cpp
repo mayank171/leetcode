c535d0921dcefdea1f2c151ff9cd163c1a1f3290
mayank <46345959+mayank171@users.noreply.github.com>
2022-03-14 16:18:35 +0530

    Time: 33 ms (72.85%), Space: 22.3 MB (99.91%) - LeetHub

---
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -0,0 +1,97 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+private:
+    TreeNode* check(TreeNode* root,int n1,int n2)
+    {
+        if(root==NULL)
+        {
+            return NULL;
+        }
+        
+        int p,q,r,s;
+        p=q=r=s=0;
+        
+        if(root->val>n1)
+        {
+            root->left=check(root->left,n1,n2);
+            p=1;
+        }
+        else if(root->val<n1)
+        {
+            root->right=check(root->right,n1,n2);
+            q=1;
+        }
+        else
+        {
+            return root;
+        }
+        
+        if(root->val>n2)
+        {
+            root->left=check(root->left,n1,n2);
+            r=1;
+        }
+        else if(root->val<n2)
+        {
+            root->right=check(root->right,n1,n2);
+            s=1;
+        }
+        else
+        {
+            return root;
+        }
+        
+        if(p==1 && r==1)
+        {
+            return root->left;
+        }
+        else if(p==1 && s==1)
+        {
+            return root;
+        }    
+        else if(q==1 && r==1)
+        {
+            return root;
+        }
+        else if(q==1 && s==1)
+        {
+            return root->right;
+        }
+        if(root->left==NULL && root->right==NULL)
+        {
+            return NULL;
+        }
+        else if(root->left!=NULL && root->right==NULL)
+        {
+            return root->left;
+        }
+        else if(root->left==NULL && root->right!=NULL)
+        {
+            return root->right;
+        }
+        else
+        {
+            return root;
+        }
+        return root;
+        
+        
+    }
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        
+        TreeNode* res=check(root,p->val,q->val);
+        
+        return res;
+        
+    }
+};
