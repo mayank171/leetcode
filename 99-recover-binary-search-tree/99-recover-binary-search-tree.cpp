2f46c3b9b9ab43a07b27af9d1d550be3d27fafec
mayank <46345959+mayank171@users.noreply.github.com>
2022-03-22 16:31:25 +0530

    Time: 24 ms (95.26%), Space: 58 MB (48.20%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
@@ -11,38 +11,79 @@
  */
 class Solution {
 private:
-    void check(TreeNode* root,vector<int> & inorder)
+    TreeNode* prev;
+    TreeNode* first;
+    TreeNode* second;
+    TreeNode* third;
+    
+//     void check(TreeNode* root,vector<int> & inorder)
+//     {
+//         if(root==NULL)
+//             return;
+//         check(root->left,inorder);
+//         inorder.push_back(root->val);
+//         check(root->right,inorder);
+//     }
+    
+//     void check2(TreeNode* root,vector<int> &inorder,int &ind)
+//     {
+//         if(root==NULL)
+//             return;
+//         check2(root->left,inorder,ind);
+//         root->val=inorder[ind];
+//         ind++;
+//         check2(root->right,inorder,ind);
+//     }
+    void inorder(TreeNode* root)
     {
         if(root==NULL)
-            return;
-        check(root->left,inorder);
-        inorder.push_back(root->val);
-        check(root->right,inorder);
+        {
+            return ;
+        }
+        
+        inorder(root->left);
+        if(prev && prev->val>root->val)
+        {
+            if(first==NULL)
+            {
+                first=prev;
+                second=root;
+            }
+            else
+            {
+                third=root;
+            }
+        }
+        
+        prev=root;
+        inorder(root->right);
     }
-    
-    void check2(TreeNode* root,vector<int> &inorder,int &ind)
-    {
-        if(root==NULL)
-            return;
-        check2(root->left,inorder,ind);
-        root->val=inorder[ind];
-        ind++;
-        check2(root->right,inorder,ind);
-    }
-    
     
 public:
     void recoverTree(TreeNode* root) {
         
-        vector<int> inorder;
         
-        check(root,inorder);
         
-        sort(inorder.begin(),inorder.end());
+//         vector<int> inorder;
         
-        int ind=0;
+//         check(root,inorder);
         
-        check2(root,inorder,ind);
+//         sort(inorder.begin(),inorder.end());
+        
+//         int ind=0;
+        
+//         check2(root,inorder,ind);
+        first=second=third=NULL;
+        prev=new TreeNode(INT_MIN);
+        
+        inorder(root);
+        
+        if(first!=NULL && third!=NULL)
+            swap(first->val,third->val);
+        else if(first!=NULL && second!=NULL)
+        {
+            swap(first->val,second->val);
+        }
         
     }
 };
