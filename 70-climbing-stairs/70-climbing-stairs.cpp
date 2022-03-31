5615531a0c38e99c60b9ad97493f31f0b780816e
mayank <46345959+mayank171@users.noreply.github.com>
2022-03-31 16:00:14 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (38.28%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,29 +1,43 @@
 class Solution {
 private:
-    int check(int n,vector<int> &dp)
-    {
-        if(n==1 || n==0)
-            return 1;
+//     int check(int n,vector<int> &dp)
+//     {
+//         if(n==1 || n==0)
+//             return 1;
         
-        if(dp[n]!=-1)
-            return dp[n];
+//         if(dp[n]!=-1)
+//             return dp[n];
         
-        int l=check(n-1,dp);
-        int r=check(n-2,dp);
+//         int l=check(n-1,dp);
+//         int r=check(n-2,dp);
         
-        return dp[n]=l+r;
+//         return dp[n]=l+r;
         
-    }
+//     }
 public:
     int climbStairs(int n) {
         
         if(n==1)
             return 1;
         
+//         vector<int> dp(n+1,-1);
+        
+//         check(n,dp);
+        
+//         return dp[n];
+        
         vector<int> dp(n+1,-1);
         
-        check(n,dp);
-        
+        for(int i=0;i<=n;i++)
+        {
+            if(i<=1)
+            {
+                dp[i]=1;
+                continue;
+            }
+            
+            dp[i]=dp[i-1]+dp[i-2];
+        }
         return dp[n];
         
     }
