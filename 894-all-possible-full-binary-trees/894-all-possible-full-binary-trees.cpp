a7889a05e963a1d0dd548187e9ef8abb845bf013
mayank <46345959+mayank171@users.noreply.github.com>
2022-04-02 15:05:53 +0530

    Time: 222 ms (12.72%), Space: 49.9 MB (16.22%) - LeetHub

--- 894-all-possible-full-binary-trees/894-all-possible-full-binary-trees.cpp
+++ 894-all-possible-full-binary-trees/894-all-possible-full-binary-trees.cpp
@@ -13,25 +13,54 @@ class Solution {
     
 private:
     
-    vector<TreeNode*> check(int n)
+//     vector<TreeNode*> check(int n)
+//     {
+//         if(n==1)
+//         {
+//             vector<TreeNode*> ans;
+//             ans.push_back(new TreeNode(0));
+//             return ans;
+//         }
+        
+//         vector<TreeNode*> ans;
+        
+//         for(int i=1;i<n;i+=2)
+//         {
+//             vector<TreeNode*> left=check(i);
+//             vector<TreeNode*> right=check(n-i-1);
+            
+//             for(auto &l:left)
+//             {
+//                 for(auto &r:right)
+//                 {
+//                     TreeNode* root=new TreeNode(0);
+//                     root->left=l;
+//                     root->right=r;
+//                     ans.push_back(root);
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+    vector<TreeNode*> check1(int n)
     {
+        vector<TreeNode*> ans;
         if(n==1)
         {
-            vector<TreeNode*> ans;
-            ans.push_back(new TreeNode(0));
+            //vector<TreeNode*> x;
+            TreeNode* node=new TreeNode(0);
+            ans.push_back(node);
             return ans;
         }
         
-        vector<TreeNode*> ans;
-        
         for(int i=1;i<n;i+=2)
         {
-            vector<TreeNode*> left=check(i);
-            vector<TreeNode*> right=check(n-i-1);
+            vector<TreeNode*> lft=check1(i);
+            vector<TreeNode*> rgt=check1(n-i-1);
             
-            for(auto &l:left)
+            for(auto &l:lft)
             {
-                for(auto &r:right)
+                for(auto &r:rgt)
                 {
                     TreeNode* root=new TreeNode(0);
                     root->left=l;
@@ -40,13 +69,20 @@ private:
                 }
             }
         }
+        
         return ans;
     }
 public:
     vector<TreeNode*> allPossibleFBT(int n) {
         
         
-       return check(n);
+//        return check(n);
+        
+        //vector<TreeNode*> ans;
+        
+        return check1(n);
+        
+        //return ans;
         
     }
 };
