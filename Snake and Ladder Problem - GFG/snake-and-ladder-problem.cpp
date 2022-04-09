bf99d974815e18f6cee1e0a5ffd53c63eab8ca82
mayank <46345959+mayank171@users.noreply.github.com>
2022-04-09 08:53:53 +0530

    Added solution - LeetHub

--- Snake and Ladder Problem - GFG/snake-and-ladder-problem.cpp
+++ Snake and Ladder Problem - GFG/snake-and-ladder-problem.cpp
@@ -12,81 +12,174 @@ public:
     int minThrow(int N, int arr[]){
         // code here
         
+        // map<int,int> ladder;
+        // map<int,int> snake;
+        // for(int i=0;i<2*N;i+=2)
+        // {
+        //     if(arr[i]<arr[i+1])
+        //       ladder.insert({arr[i],arr[i+1]});
+        //     else
+        //       snake.insert({arr[i],arr[i+1]});
+        // }
+        
+        // queue<pair<int,int>> q;
+        // q.push({1,0});
+        
+        // int ct=INT_MAX;
+        // vector<int> vis(31,0);
+        // vis[1]=1;
+        
+        // while(!q.empty())
+        // {
+        //     int x=q.front().first;
+        //     int moves=q.front().second;
+        //     q.pop();
+            
+        //     //cout<<x<<" "<<moves<<endl;
+            
+        //     if(x==30)
+        //     {
+        //         ct=min(ct,moves);
+        //         break;
+        //     }
+            
+            
+        //     for(int i=1;i<=6;i++)
+        //     {
+        //         int temp=x+i;
+                
+        //         if(temp>30)
+        //           break;
+                
+        //         if(vis[temp]==1)
+        //           continue;
+             
+                  
+        //         auto it=ladder.find(temp);
+        //         if(it!=ladder.end())
+        //         {
+                    
+        //             temp=(*it).second;
+        //             if(vis[temp]==1)
+        //               continue;
+        //         }
+        //         auto it1=snake.find(temp);
+        //         if(it1!=snake.end())
+        //         {
+                    
+        //             temp=(*it1).second;
+        //             if(vis[temp]==1)
+        //             continue;
+        //         }
+         
+        //         if(temp<=30)
+        //         {
+        //             vis[temp]=1;
+        //             q.push({temp,moves+1});
+        //         }
+        //         // else if(temp==30)
+        //         // {
+        //         //     vis[temp]=1;
+        //         //     q.push({temp,moves});
+        //         // }
+                   
+        //     }
+        // }
+        
+        // return ct;
+        
         map<int,int> ladder;
         map<int,int> snake;
-        for(int i=0;i<2*N;i+=2)
+        int i;
+        for(i=0;i<N-1;i+=2)
         {
-            if(arr[i]<arr[i+1])
-               ladder.insert({arr[i],arr[i+1]});
-            else
-               snake.insert({arr[i],arr[i+1]});
+            ladder[arr[i]]=arr[i+1];
         }
+        
+        for(int j=i;j<2*N-1;j+=2)
+        {
+            snake[arr[j]]=arr[j+1];
+        }
+        
+        // for(auto &it:ladder)
+        // {
+        //     cout<<it.first<<" "<<it.second<<endl;
+        // }
+        
+        // for(auto &it:snake)
+        // {
+        //     cout<<it.first<<" "<<it.second<<endl;
+        // }
+        
         
         queue<pair<int,int>> q;
         q.push({1,0});
         
-        int ct=INT_MAX;
         vector<int> vis(31,0);
+        
+        int ans=INT_MAX;
+        
         vis[1]=1;
         
         while(!q.empty())
         {
-            int x=q.front().first;
+            int start=q.front().first;
             int moves=q.front().second;
             q.pop();
+           // cout<<start<<endl;
             
-            //cout<<x<<" "<<moves<<endl;
-            
-            if(x==30)
+            if(start==30)
             {
-                ct=min(ct,moves);
-                break;
+                ans=min(ans,moves);
+                continue;
             }
             
+            if(start>30)
+            {
+                continue;
+            }
             
             for(int i=1;i<=6;i++)
             {
-                int temp=x+i;
+                int pos=start+i;
                 
-                if(temp>30)
-                   break;
-                
-                if(vis[temp]==1)
+                if(pos>30)
                   continue;
-             
-                  
-                auto it=ladder.find(temp);
-                if(it!=ladder.end())
+                
+                if(vis[pos]==1)
                 {
-                    
-                    temp=(*it).second;
-                    if(vis[temp]==1)
-                      continue;
-                }
-                auto it1=snake.find(temp);
-                if(it1!=snake.end())
-                {
-                    
-                    temp=(*it1).second;
-                    if(vis[temp]==1)
                     continue;
                 }
-         
-                if(temp<=30)
+                if(ladder.find(pos)!=ladder.end())
                 {
-                    vis[temp]=1;
-                    q.push({temp,moves+1});
+                    if(vis[ladder[pos]]==1)
+                    continue;
+                    
+                    pos=ladder[pos];
+                    vis[pos]=1;
+                    q.push({pos,moves+1});
                 }
-                // else if(temp==30)
-                // {
-                //     vis[temp]=1;
-                //     q.push({temp,moves});
-                // }
-                   
+                if(snake.find(pos)!=snake.end())
+                {
+                    if(vis[snake[pos]]==1)
+                    continue;
+                    
+                    pos=snake[pos];
+                    vis[pos]=1;
+                    q.push({pos,moves+1});
+                }
+                
+                vis[pos]=1;
+                q.push({pos,moves+1});
+                
+                
             }
+            
+            
         }
         
-        return ct;
+        return ans;
+        
         
         
     }
