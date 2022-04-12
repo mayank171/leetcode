fc3b2813e11570acbc1a7cb3b487f4c23e645a55
mayank <46345959+mayank171@users.noreply.github.com>
2022-04-12 18:02:23 +0530

    Time: 41 ms (6.76%), Space: 7.2 MB (34.12%) - LeetHub

---
+++ 331-verify-preorder-serialization-of-a-binary-tree/331-verify-preorder-serialization-of-a-binary-tree.cpp
@@ -0,0 +1,95 @@
+class Solution {
+public:
+    bool isValidSerialization(string preorder) {
+        
+        int ct_hash=0;
+        
+        int ct_num=0;
+        
+        int l=preorder.length();
+        
+        vector<int> v;
+        
+        for(int i=0;i<l;i++)
+        {
+            if(preorder[i]==',')
+                continue;
+            if(preorder[i]>='0' && preorder[i]<='9')
+            {
+                int k=0;
+                int j=i;
+                while(j<l && preorder[j]>='0' && preorder[j]<='9')
+                {
+                    k=k*10+preorder[j]-'0';
+                    j++;
+                }
+                
+                v.push_back(k);
+                i=j-1;
+            }
+            else
+            {
+                v.push_back(-1);
+            }
+        }
+        
+        for(auto &it:v)
+        {
+            cout<<it<<" ";
+        }
+        
+        if(v[0]==-1)
+        {
+            if(l>1)
+                return false;
+            return true;
+        }
+        else
+        {
+            if(v.size()==1)
+                return false;
+        }
+        
+        
+        
+        int ct=1;
+        
+        int size=v.size();
+        
+        if(v[size-2]!=-1)
+            return false;
+        
+        if(v[size-1]!=-1)
+            return false;
+        
+        
+        
+        
+        for(int i=0;i<size;i++)
+        {
+            if(v[i]!=-1)
+            {
+                ct--;
+                if(ct<0)
+                    return false;
+                ct+=2;
+            }
+            else
+            {
+                ct--;
+            }
+            
+            if(ct<0)
+                return false;
+        }
+        
+        
+        if(ct==0)
+        {
+            return true;
+        }
+        
+        return false;
+        
+    }
+};
