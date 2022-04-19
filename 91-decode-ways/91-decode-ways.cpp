d42ace7d16efc7ccab41c3915e0e6d87db6650d2
mayank <46345959+mayank171@users.noreply.github.com>
2022-04-19 14:37:50 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (73.36%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,69 +1,48 @@
 class Solution {
 private:
     
-//     int check(string &s,int n,vector<int> &dp)
+    
+    
+//     int check(int ind,int n,string &s,vector<int> &dp)
 //     {
-        
-//         if(n==0)
+//         if(ind==n)
 //         {
 //             return dp[n]=1;
 //         }
         
-//         if(dp[n]!=-1)
-//             return dp[n];
+//         if(dp[ind]!=-1)
+//             return dp[ind];
         
 //         int ct=0;
-//         if(s[n-1]!='0')
+    
+//         if(s[ind]!='0')
 //         {
-//             ct+=check(s,n-1,dp);
+//             ct+=check(ind+1,n,s,dp);
+//         }
+//         else
+//         {
+//             return dp[ind]=0;
 //         }
         
-//         if(n>1 && stoi(s.substr(n-2,2))>=10 && stoi(s.substr(n-2,2))<=26)
+//         if(ind+1<n && (s[ind]-'0')*10+(s[ind+1]-'0')>0 && (s[ind]-'0')*10+(s[ind+1]-'0')<=26)
 //         {
-//             ct+=check(s,n-2,dp);
+//             ct+=check(ind+2,n,s,dp);
 //         }
-        
-//         return dp[n]=ct;
-//     }
-    
-    
-    int check(int ind,int n,string &s,vector<int> &dp)
-    {
-        if(ind==n)
-        {
-            return dp[n]=1;
-        }
-        
-        if(dp[ind]!=-1)
-            return dp[ind];
-        
-        int ct=0;
-    
-        if(s[ind]!='0')
-        {
-            ct+=check(ind+1,n,s,dp);
-        }
-        else
-        {
-            return dp[ind]=0;
-        }
-        
-        if(ind+1<n && (s[ind]-'0')*10+(s[ind+1]-'0')>0 && (s[ind]-'0')*10+(s[ind+1]-'0')<=26)
-        {
-            ct+=check(ind+2,n,s,dp);
-        }
        
-        return dp[ind]=ct;
-    }
+//         return dp[ind]=ct;
+//     }
     
 public:
     int numDecodings(string s) {
         
-//         int l=s.length();
         
-//         vector<int> dp(l+1,-1);
+//         int n=s.length();
         
-//         int ans=check(s,l,dp);
+//         vector<int> dp(n+1,-1);
+        
+//         int ind=0;
+        
+//         check(ind,n,s,dp);
         
         
 //         for(auto &it:dp)
@@ -72,25 +51,51 @@ public:
 //         }
 //         cout<<endl;
         
-//         return ans;
         
+//         return dp[0];
         
         int n=s.length();
         
-        vector<int> dp(n+1,-1);
+        vector<int> dp(n+1,0);
         
-        int ind=0;
-        
-        check(ind,n,s,dp);
+        dp[n]=1;
         
         
-        for(auto &it:dp)
+        
+        
+        for(int i=n-1;i>=0;i--)
         {
-            cout<<it<<" ";
+            if(s[i]>'2')
+            {
+                dp[i]=dp[i+1];
+            }
+            else if(s[i]=='0')
+            {
+                continue;
+            }
+            else
+            {
+                if(i+2>=n+1)
+                {
+                    dp[i]=dp[i+1];
+                }
+                else
+                {
+                    if(s[i+1]>'6' && s[i]=='2')
+                    {
+                        dp[i]=dp[i+2];
+                    }
+                    else 
+                    {
+                        dp[i]=dp[i+1]+dp[i+2];
+                    }
+                    
+                }
+                  
+            }
         }
-        cout<<endl;
-        
         
         return dp[0];
+        
     }
 };
