aefe32c8f99e505ee4dcbf6c377732b2fb22e67f
mayank <46345959+mayank171@users.noreply.github.com>
2022-04-23 17:21:26 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (95.12%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -42,40 +42,64 @@ public:
 //         return dp[n-1];
         
         
+//         int n=nums.size();
+        
+//         vector<int> dp(n,-1);
+        
+//         dp[0]=nums[0];
+//         int neg=0;
+        
+//         for(int i=1;i<n;i++)
+//         {
+//             int take=0;
+//             int not_take=0;
+//             if(i-2<0)
+//             {
+//                 take=nums[i]+neg;
+//             }
+//             else
+//             {
+//                 take=nums[i]+dp[i-2];
+//             }
+            
+//             not_take=0+dp[i-1];
+            
+//             dp[i]=max(take,not_take);
+            
+           
+            
+//         }
+        
+//         return dp[n-1];
+        
+        
         int n=nums.size();
         
-        vector<int> dp(n,-1);
-        
-        dp[0]=nums[0];
-        int neg=0;
+        int prev1=nums[0];
+        int prev2=0;
         
         for(int i=1;i<n;i++)
         {
             int take=0;
             int not_take=0;
-            if(i-2<0)
+            
+            if(i-2>=0)
             {
-                take=nums[i]+neg;
+                take=nums[i]+prev2;
             }
             else
             {
-                take=nums[i]+dp[i-2];
+                take=nums[i];
             }
             
-            not_take=0+dp[i-1];
+            not_take=prev1;
             
-            dp[i]=max(take,not_take);
-            
-           
-            
+            int cur=max(take,not_take);
+            prev2=prev1;
+            prev1=cur;
         }
         
-        return dp[n-1];
-        
-        
-        
-        
-        
+        return max(prev1,prev2);
         
         
         
