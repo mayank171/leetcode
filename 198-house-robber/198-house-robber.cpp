79e7c00ef4feca5cb29e7030abfc6a3fab156859
mayank <46345959+mayank171@users.noreply.github.com>
2022-04-23 16:11:03 +0530

    Time: 2 ms (52.84%), Space: 7.8 MB (25.58%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,39 +1,85 @@
 class Solution {
 private:
     
-    int check(vector<int> &nums,vector<int> &dp,int ind)
-    {
-        if(ind==0)
-        {
-            return dp[ind]=nums[ind];
-        }
+//     int check(vector<int> &nums,vector<int> &dp,int ind)
+//     {
+//         if(ind==0)
+//         {
+//             return dp[ind]=nums[ind];
+//         }
         
-        if(ind<0)
-        {
-            return 0;
-        }
+//         if(ind<0)
+//         {
+//             return 0;
+//         }
         
-        if(dp[ind]!=-1)
-            return dp[ind];
+//         if(dp[ind]!=-1)
+//             return dp[ind];
         
-        int sum1=nums[ind]+check(nums,dp,ind-2);
-        int sum2=0+check(nums,dp,ind-1);
+//         int sum1=nums[ind]+check(nums,dp,ind-2);
+//         int sum2=0+check(nums,dp,ind-1);
         
-        return dp[ind]=max(sum1,sum2);
-    }
+//         return dp[ind]=max(sum1,sum2);
+//     }
     
 public:
     int rob(vector<int>& nums) {
+        
+//         int n=nums.size();
+        
+//         vector<int> dp(n,-1);
+        
+//         int ind=n-1;
+        
+//         check(nums,dp,ind);
+        
+//         for(auto &it:dp)
+//         {
+//             cout<<it<<" ";
+//         }
+//         cout<<endl;
+        
+//         return dp[n-1];
+        
         
         int n=nums.size();
         
         vector<int> dp(n,-1);
         
-        int ind=n-1;
+        dp[0]=nums[0];
+        int neg=0;
         
-        check(nums,dp,ind);
+        for(int i=1;i<n;i++)
+        {
+            int take=0;
+            int not_take=0;
+            if(i-2<0)
+            {
+                take=nums[i]+neg;
+            }
+            else
+            {
+                take=nums[i]+dp[i-2];
+            }
+            
+            not_take=0+dp[i-1];
+            
+            dp[i]=max(take,not_take);
+            
+           
+            
+        }
         
         return dp[n-1];
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
     }
 };
