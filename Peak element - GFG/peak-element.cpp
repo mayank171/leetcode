865dc0fe2f27585a3173f601b6e6d334042a63b0
mayank <46345959+mayank171@users.noreply.github.com>
2022-04-28 21:29:03 +0530

    Added solution - LeetHub

---
+++ Peak element - GFG/peak-element.cpp
@@ -0,0 +1,96 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+/* The function should return the index of any
+   peak element present in the array */
+
+// arr: input array
+// n: size of array
+class Solution
+{
+    public:
+    int peakElement(int arr[], int n)
+    {
+       // Your code here
+       int low=0;
+       int  high=n-1;
+       
+       
+       if(n==1)
+       {
+           return 0;
+       }
+       
+       
+       while(low<high)
+       {
+           int mid=low+(high-low)/2;
+           
+        //   if(abs(low-high)==1)
+        //       break;
+           
+           
+           if(arr[mid]<arr[mid+1])
+           {
+               low=mid+1;
+           }
+           else
+           {
+               high=mid;
+           }
+       }
+       
+       return low;
+       
+    //   int ans;
+       
+    //   arr[low]<arr[high]?ans=high:ans=low;
+       
+    //   return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int a[n], tmp[n];
+		for(int i=0;i<n;i++)
+		{
+			cin>>a[i];
+			tmp[i] = a[i];
+		}
+		bool f=0;
+		Solution ob;
+		
+		int A = ob. peakElement(tmp,n);
+		
+		if(A<0 and A>=n)
+		    cout<<0<<endl;
+		else
+		{
+    		if(n==1 and A==0)
+    		    f=1;
+    		else if(A==0 and a[0]>=a[1])
+    		    f=1;
+    		else if(A==n-1 and a[n-1]>=a[n-2])
+    		    f=1;
+    		else if(a[A]>=a[A+1] and a[A]>= a[A-1])
+    		    f=1;
+    		else
+    		    f=0;
+    		cout<<f<<endl;
+		}
+		
+	}
+
+	return 0;
+}  // } Driver Code Ends
