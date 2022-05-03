2f2d7ad7a30f5a95dbe34eb1b1f32a8ee920338a
mayank <46345959+mayank171@users.noreply.github.com>
2022-05-03 14:54:46 +0530

    Time: 9 ms (49.62%), Space: 13.8 MB (50.99%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -1,45 +1,72 @@
 class Solution {
 private:
     
-     int check(vector<int> &cost,int n,vector<int> &dp)
-     {
-         if(n==1)
-         {
-             return dp[n]=cost[1];
-         }
+//      int check(vector<int> &cost,int n,vector<int> &dp)
+//      {
+//          if(n==1)
+//          {
+//              return dp[n]=cost[1];
+//          }
          
-         if(n==0)
-         {
-             return dp[n]=cost[0];
-         }
+//          if(n==0)
+//          {
+//              return dp[n]=cost[0];
+//          }
          
-         if(dp[n]!=-1)
-         {
-             return dp[n];
-         }
+//          if(dp[n]!=-1)
+//          {
+//              return dp[n];
+//          }
          
-         return dp[n]=min(check(cost,n-1,dp),check(cost,n-2,dp))+cost[n];
+//          return dp[n]=min(check(cost,n-1,dp),check(cost,n-2,dp))+cost[n];
          
-     }
+//      }
     
 public:
     int minCostClimbingStairs(vector<int>& cost) {
         
+//         int n=cost.size();
+        
+//         cost.push_back(0);
+        
+//         vector<int> dp(n+1,-1);
+        
+//         check(cost,n,dp);
+        
+//         for(auto &it:dp)
+//         {
+//             cout<<it<<" ";
+//         }
+//         cout<<endl;
+        
+//         return min(dp[n-1],dp[n-2]);
+        
         int n=cost.size();
         
-        cost.push_back(0);
-        
-        vector<int> dp(n+1,-1);
-        
-        check(cost,n,dp);
-        
-        for(auto &it:dp)
+        if(n==2)
         {
-            cout<<it<<" ";
+            return min(cost[0],cost[1]);
         }
-        cout<<endl;
         
-        return min(dp[n-1],dp[n-2]);
+        vector<int> dp(n,0);
+        
+        dp[0]=cost[0];
+        
+        for(int i=1;i<n;i++)
+        {
+            if(i-2<0)
+                dp[i]=cost[i]+min(dp[i-1],0);
+            else
+                dp[i]=cost[i]+min(dp[i-1],dp[i-2]);
+        }
+        
+        // for(auto &it:dp)
+        // {
+        //     cout<<it<<" ";
+        // }
+        // cout<<endl;
+        
+        return min(dp[n-2],dp[n-1]);
         
     }
 };
