5595c0d1c1f779e166f03708981ed4f08280ed6e
mayank <46345959+mayank171@users.noreply.github.com>
2022-05-19 16:53:27 +0530

    Time: 8 ms (27.14%), Space: 7.3 MB (40.95%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/410-split-array-largest-sum.cpp
@@ -1,52 +1,51 @@
 class Solution {
 private:
-    int blackBox(vector<int> &nums,int max_sum,int m)
-    {
-        int n=nums.size();
-        
-        int sum=0;
-        
-        int ct=0;
-        
-        for(int i=0;i<n;i++)
-        {
-            sum+=nums[i];
-            if(sum>max_sum)
-            {
-                sum=0;
-                ct++;
-                if(ct>m)
-                    return ct;
-                i--;
-                continue;
-            }
-        }
-        
-        if(sum>0)
-            ct++;
-        return ct;
-    }
+   int blackBox(vector<int> &nums,int n,int m,int mid)
+   {
+       int sum=0;
+       int maxi=0;
+       for(int i=0;i<n;i++)
+       {
+           if(sum+nums[i]<=mid)
+           {
+               sum+=nums[i];
+           }
+           else
+           {
+               maxi++;
+               if(maxi>m)
+                   return maxi;
+               sum=0;
+               i--;
+           }
+       }
+       
+       if(0<sum)
+       {
+           maxi++;
+       }
+       
+       return maxi;
+       
+   }
 public:
     int splitArray(vector<int>& nums, int m) {
         
         int n=nums.size();
         
         int low=*max_element(nums.begin(),nums.end());
+        int high=accumulate(nums.begin(),nums.end(),0);
         
-        int high=0;
         
-        for(int i=0;i<n;i++)
-        {
-            high+=nums[i];
-        }
-        
-        int ans=high;
-        
+        int ans=low;
         while(low<=high)
         {
             int mid=low+(high-low)/2;
+           // cout<<mid<<endl;
             
-            if(blackBox(nums,mid,m)>m)
+            int val=blackBox(nums,n,m,mid);
+            
+            if(val>m)
             {
                 low=mid+1;
             }
