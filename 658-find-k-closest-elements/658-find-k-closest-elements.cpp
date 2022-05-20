a3a1e89c01055b6be7b2e37177ab3acd88079fe7
mayank <46345959+mayank171@users.noreply.github.com>
2022-05-20 17:02:17 +0530

    Time: 66 ms (54.13%), Space: 31.8 MB (44.45%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,41 +1,60 @@
 class Solution {
 public:
+    
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
         
         int n=arr.size();
         
-        int low=0;
-        int high=n-k;
+        int close=0;
         
-        int ans=INT_MAX;
+        stable_sort(arr.begin(),arr.end(),[x](const auto a,const auto b){
+            return abs(a-x)<abs(b-x);
+        });
         
-        while(low<high)
-        {
-            int mid=low+(high-low)/2;
+        
+        arr.resize(k);
+        
+        
+        sort(arr.begin(),arr.end());
+        return arr;
+        
+        
+        
+        
+//         int n=arr.size();
+        
+//         int low=0;
+//         int high=n-k;
+        
+//         int ans=INT_MAX;
+        
+//         while(low<high)
+//         {
+//             int mid=low+(high-low)/2;
             
-            if(x-arr[mid] > arr[mid+k]-x)
-            {
-                low=mid+1;
-            }
-            else
-            {
-                high=mid;
-            }
-        }
+//             if(x-arr[mid] > arr[mid+k]-x)
+//             {
+//                 low=mid+1;
+//             }
+//             else
+//             {
+//                 high=mid;
+//             }
+//         }
         
-        vector<int> res;
+//         vector<int> res;
         
-        int ind=low;
+//         int ind=low;
         
-        while(k--)
-        {
-            res.push_back(arr[ind]);
-            ind++;
-        }
+//         while(k--)
+//         {
+//             res.push_back(arr[ind]);
+//             ind++;
+//         }
 
-        //sort(res.begin(),res.end());
         
-        return res;
+        
+//         return res;
         
         
     }
