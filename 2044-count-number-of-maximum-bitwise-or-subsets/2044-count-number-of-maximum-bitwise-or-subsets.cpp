0384a96726c249371439cbee4c71d45d1b3273f1
mayank <46345959+mayank171@users.noreply.github.com>
2022-05-24 13:48:16 +0530

    Time: 832 ms (7.72%), Space: 200 MB (8.95%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,109 +1,74 @@
 class Solution {
-    
 private:
-//     int subsets(vector<int> &nums,vector<vector<int>> &ans,int N,int maxOR)
-//     {
-//         int size=(1<<N);
-        
-//         int ct=0;
-//         for(int i=0;i<size;i++)
-//         {
-//             vector<int> temp;
-//             int maxor=0;
-//             for(int j=0;j<N;j++)
-//             {
-//                 if(i&(1<<j))
-//                 {
-//                     maxor|=nums[j];
-//                 }
-//             }
-//             if(maxor==maxOR)
-//                 ct++;
-//         }
-//         return ct;
-//     }
-  
-    void check(int n,int &ct,vector<int> &nums,int maxor)
+    void check(vector<int> &nums,int n,int ind,vector<vector<int>> &ans,vector<int> &temp,int &maxi)
     {
-        int size=(1<<n);
-        
-        for(int i=0;i<size;i++){
-
-             int x=0;
-             for(int j=0;j<n;j++)
-             {
-                 if(i&(1<<j))
-                 {
-                    x=x|(nums[j]); 
-                 }
-             }
+        if(ind==n)
+        {
+//             int x=temp.size();
+//             int ori=0;
+//             for(int i=0;i<x;i++)
+//             {
+//                 ori|=temp[i];
+//             }
             
-            if(maxor==x)
-                ct++;
-    
+//             if(ori>maxi)
+//                 maxi=ori;
+            
+            ans.push_back(temp);
+            return ;
         }
         
-        
+        temp.push_back(nums[ind]);
+        check(nums,n,ind+1,ans,temp,maxi);
+        temp.pop_back();
+        check(nums,n,ind+1,ans,temp,maxi);
+
     }
-    
-    
 public:
     int countMaxOrSubsets(vector<int>& nums) {
-  
         
-//         vector<vector<int>> ans;
+
         
-//         vector<int> temp;
-        
-//         int ind=0;
-        
-//         int N=nums.size();
-        
-//         int maxOR=0;
-        
-//         int ct=0;
-        
-//         for(int i=0;i<N;i++)
-//         {
-//             maxOR=maxOR|nums[i];
-//         }
-        
-//         ct=subsets(nums,ans,N,maxOR);
-        
-//         int size=ans.size();
-        
-        
-        
-// //         for(int i=0;i<size-1;i++)
-// //         {
-// //             int maxor=0;
-// //             for(int j=0;j<ans[i].size();j++)
-// //             {
-// //                 maxor=maxor|ans[i][j];
-// //             }
-// //             if(maxor==maxOR)
-// //                 ct++;
-// //         }
-        
-//         return ct;
-        
+        vector<vector<int>> ans;
+        vector<int> temp;
         
         int n=nums.size();
         
         int max_or=0;
+
+        int ind=0;
         
-        for(int i=0;i<n;i++)
+        check(nums,n,ind,ans,temp,max_or);
+        
+        int m=ans.size();
+        int ct=0;
+        
+        for(int i=0;i<m;i++)
         {
-            max_or|=nums[i];
+            int ori=0;
+            for(int j=0;j<ans[i].size();j++)
+            {
+                ori|=ans[i][j];
+            }
+            
+            if(ori>max_or)
+                max_or=ori;
         }
         
         
-        int ct=0;
-        
-        check(n,ct,nums,max_or);
+        for(int i=0;i<m;i++)
+        {
+            int ori=0;
+            for(int j=0;j<ans[i].size();j++)
+            {
+                ori|=ans[i][j];
+            }
+            
+            if(ori==max_or)
+                ct++;
+        }
         
         return ct;
-        
         
     }
 };
