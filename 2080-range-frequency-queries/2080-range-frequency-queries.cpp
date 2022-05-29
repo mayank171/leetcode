67416c8721d7af3b61987d76084375c67518a74e
mayank <46345959+mayank171@users.noreply.github.com>
2022-05-29 10:38:36 +0530

    Time: 892 ms (40.53%), Space: 235.6 MB (64.80%) - LeetHub

--- 2080-range-frequency-queries/2080-range-frequency-queries.cpp
+++ 2080-range-frequency-queries/2080-range-frequency-queries.cpp
@@ -1,32 +1,25 @@
 class RangeFreqQuery {
 private:
     unordered_map<int,vector<int>> mp;
-    unordered_map<int,int> mp1;
     
 public:
     RangeFreqQuery(vector<int>& arr) {
-        int size=arr.size();
         
-        for(int i=0;i<size;i++)
+        int n=arr.size();
+        
+        for(int i=0;i<n;i++)
         {
-           mp[arr[i]].push_back(i);     
+            mp[arr[i]].push_back(i);
         }
         
-        for(auto &it:mp)
-        {
-            cout<<it.first<<endl;
-            mp1[it.first]=it.second.size();
-        }
     }
     
     int query(int left, int right, int value) {
+      
+        int ind1=lower_bound(mp[value].begin(),mp[value].end(),left)-mp[value].begin();
+        int ind2=upper_bound(mp[value].begin(),mp[value].end(),right)-mp[value].begin();
         
-        int back=upper_bound(mp[value].begin(),mp[value].end(),right)-mp[value].begin();
-        int front=lower_bound(mp[value].begin(),mp[value].end(),left)-mp[value].begin();
-        
-        return back-front;
-        
-        
+        return ind2-ind1;
     }
 };
 
