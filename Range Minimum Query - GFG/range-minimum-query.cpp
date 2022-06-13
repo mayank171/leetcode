50b516dd0e4ba267e4e5a929253e49638e84f5bc
mayank <46345959+mayank171@users.noreply.github.com>
2022-06-13 17:36:27 +0530

    Added solution - LeetHub

--- Range Minimum Query - GFG/range-minimum-query.cpp
+++ Range Minimum Query - GFG/range-minimum-query.cpp
@@ -41,39 +41,37 @@ int main()
 // } Driver Code Ends
 
 vector<int> vp(1e7,INT_MAX);
-  
-void build(int arr[],vector<int> &vp,int si,int ss,int se)
-{
-    
-    
-    if(ss==se)
-    {
-        vp[si]=arr[ss];
-        return;
-    }
-    
-    int mid=(ss+se)/2;
-    
-    build(arr,vp,2*si+1,ss,mid);
-    build(arr,vp,2*si+2,mid+1,se);
-    
-    vp[si]=min(vp[2*si+1],vp[2*si+2]);
-}
 
 int query(int qs,int qe,int si,int ss,int se)
 {
     if(qs>se || qe<ss)
        return INT_MAX;
-    
-    if(qs<=ss && se<=qe)
+       
+    if(ss>=qs && se<=qe)
        return vp[si];
        
     int mid=(ss+se)/2;
-    
+       
     int l=query(qs,qe,2*si+1,ss,mid);
     int r=query(qs,qe,2*si+2,mid+1,se);
     
     return min(l,r);
+}
+
+void build(int arr[],int n,int si,int ss,int se)
+{
+    if(ss==se)
+    {
+        vp[si]=arr[ss];
+        return ;
+    }
+    
+    int mid=(ss+se)/2;
+    
+    build(arr,n,2*si+1,ss,mid);
+    build(arr,n,2*si+2,mid+1,se);
+    
+    vp[si]=min(vp[2*si+1],vp[2*si+2]);
 }
 
 /* The functions which 
@@ -82,10 +80,7 @@ int *constructST(int arr[],int n)
 {
   //Your code here
   
-  
-  build(arr,vp,0,0,n-1);
-  
-  
+  build(arr,n,0,0,n-1);
   
 }
 
@@ -95,7 +90,6 @@ int *constructST(int arr[],int n)
  from a and b */
 int RMQ(int st[] , int n, int a, int b)
 {
-//Your code here
-   int ans=query(a,b,0,0,n-1);
-   return ans;
+    int ans=query(a,b,0,0,n-1);
+    return ans;
 }
