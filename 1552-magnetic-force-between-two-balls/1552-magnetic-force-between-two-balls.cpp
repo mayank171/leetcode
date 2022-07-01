114b3bf8513ddd1f57e45422f56fc711d94ae154
mayank <46345959+mayank171@users.noreply.github.com>
2022-07-01 17:30:12 +0530

    Time: 613 ms (5.05%), Space: 105.4 MB (8.22%) - LeetHub

---
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    int blackBox(vector<int> position,int n,int mid)
+    {
+        int ct=1;
+        int pos=position[0];
+        
+        while(true)
+        {
+            pos+=mid;
+            int ind=upper_bound(position.begin(),position.end(),pos)-position.begin();
+          //  cout<<pos<<" "<<mid<<" "<<ind<<endl;
+            if(ind>0 && position[ind-1]==pos)
+            {
+                ct++;
+                pos=position[ind-1];
+            }
+            else if(ind<n)
+            {
+                ct++;
+                pos=position[ind];
+            }
+            else if(ind==n)
+            {
+                break;
+            }
+        }
+        return ct;
+    }
+    
+    int maxDistance(vector<int>& position, int m) {
+        
+        int n=position.size();
+        
+        sort(position.begin(),position.end());
+        
+        int low=1;
+        int high=position[n-1];
+        int ans=high;
+        
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+           // cout<<"mid"<<mid<<endl;
+            int k=blackBox(position,n,mid);
+           // cout<<k<<endl;
+            if(k>=m)
+            {
+                ans=mid;
+                low=mid+1;
+            }
+            else
+            {
+                high=mid-1;
+            }
+        }
+        return ans;
+        
+    }
+};
