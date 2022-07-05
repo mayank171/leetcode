19e10d7392a8df8e044e272e0f06a9b259f16509
mayank <46345959+mayank171@users.noreply.github.com>
2022-07-05 15:39:56 +0530

    Time: 69 ms (34.32%), Space: 20.8 MB (33.10%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -1,56 +1,31 @@
 class Solution {
-private:
-    static bool comp(vector<string> a,vector<string> b)
-    {
-        if(a.size()<b.size())
-            return true;
-        return false;
-    }
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         
-        
-        
-        map<vector<int>,vector<string>> mp;
+        map<string,vector<string>> mp;
         
         int n=strs.size();
         
         for(int i=0;i<n;i++)
         {
-            
-            
-            vector<int> freq(26,0);
-            int len=strs[i].length();
-            
-            for(int j=0;j<len;j++)
-            {
-                freq[strs[i][j]-'a']++;    
-            }
-            
-            mp[freq].push_back(strs[i]);
+            string s=strs[i];
+            sort(strs[i].begin(),strs[i].end());
+            mp[strs[i]].push_back(s);
         }
         
         vector<vector<string>> ans;
         
-        
         for(auto &it:mp)
         {
-            auto itr=it.second;
             vector<string> temp;
-            for(auto &it1:itr)
+            for(auto &it1:it.second)
             {
-                cout<<it1<<" ";
                 temp.push_back(it1);
             }
             ans.push_back(temp);
-            cout<<endl;
         }
         
-        sort(ans.begin(),ans.end(),comp);
-        
         return ans;
-            
-        
         
     }
 };
