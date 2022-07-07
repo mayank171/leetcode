d1e8ee3c42c7bb1ae3c61a41108d2ecff24b1f4e
mayank <46345959+mayank171@users.noreply.github.com>
2022-07-07 10:31:46 +0530

    Time: 63 ms (12.13%), Space: 13.3 MB (10.59%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,6 +1,6 @@
 class Solution {
 private:
-    int check(string text1,string text2,vector<vector<int>> &dp,int ind1,int ind2)
+    int check(string &text1,string &text2,vector<vector<int>> &dp,int ind1,int ind2)
     {
         if(ind1<0 || ind2<0)
             return 0;
@@ -21,39 +21,39 @@ public:
         int ind2=text2.length();
         
         
-        vector<vector<int>> dp(ind1+1,vector<int>(ind2+1,0));
+        vector<vector<int>> dp(ind1,vector<int>(ind2,-1));
         
-        //return check(text1,text2,dp,ind1-1,ind2-1);
+        return check(text1,text2,dp,ind1-1,ind2-1);
         
         
-        for(int i=1;i<ind1+1;i++)
-        {
-            for(int j=1;j<ind2+1;j++)
-            {
-                if(text1[i-1]==text2[j-1])
-                {
-                    dp[i][j]=1+dp[i-1][j-1];
-                }
-                else
-                {
-                    int x=dp[i-1][j];
-                    int y=dp[i][j-1];
-                    dp[i][j]=max(x,y);
-                }
-            }
+//         for(int i=1;i<ind1+1;i++)
+//         {
+//             for(int j=1;j<ind2+1;j++)
+//             {
+//                 if(text1[i-1]==text2[j-1])
+//                 {
+//                     dp[i][j]=1+dp[i-1][j-1];
+//                 }
+//                 else
+//                 {
+//                     int x=dp[i-1][j];
+//                     int y=dp[i][j-1];
+//                     dp[i][j]=max(x,y);
+//                 }
+//             }
             
-        }
+//         }
         
-        // for(int i=0;i<ind1+1;i++)
-        // {
-        //     for(int j=0;j<ind2+1;j++)
-        //     {
-        //         cout<<dp[i][j]<<" ";
-        //     }
-        //     cout<<endl;
-        // }
+//         // for(int i=0;i<ind1+1;i++)
+//         // {
+//         //     for(int j=0;j<ind2+1;j++)
+//         //     {
+//         //         cout<<dp[i][j]<<" ";
+//         //     }
+//         //     cout<<endl;
+//         // }
         
-        return dp[ind1][ind2];
+//         return dp[ind1][ind2];
         
         
     }
