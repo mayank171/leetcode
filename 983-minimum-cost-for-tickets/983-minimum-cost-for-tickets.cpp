8f58c71b0dff20115857850e1484656712172481
mayank <46345959+mayank171@users.noreply.github.com>
2022-07-11 12:24:31 +0530

    Time: 4 ms (75.56%), Space: 10.1 MB (15.55%) - LeetHub

---
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+   
+    int check(vector<int> &days,int n,vector<int> &costs,int ind,set<int> &st,vector<int> &dp)
+    {
+        if(ind>days[n-1])
+            return 0;
+        
+        
+        if(st.find(ind)==st.end())
+        {
+            //cout<<"~~~~~"<<endl;
+            return check(days,n,costs,ind+1,st,dp);
+        }
+        
+        if(dp[ind]!=-1)
+            return dp[ind];
+        
+        int mini=1e8;
+        for(int i=0;i<3;i++)
+        {
+            if(i==0)
+               mini=min(mini,costs[i]+check(days,n,costs,ind+1,st,dp));
+            else if(i==1)
+               mini=min(mini,costs[i]+check(days,n,costs,ind+7,st,dp));
+            else if(i==2)
+               mini=min(mini,costs[i]+check(days,n,costs,ind+30,st,dp));
+           // cout<<mini<<endl;
+        }
+      //  cout<<"---"<<endl;
+        return dp[ind]=mini;
+    }
+    
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        
+        int n=days.size();
+        
+        set<int> st;
+        
+        for(int i=0;i<n;i++)
+            st.insert(days[i]);
+        
+        vector<int> dp(days[n-1]+1,-1);
+        
+        return check(days,n,costs,0,st,dp);
+        
+    }
+};
