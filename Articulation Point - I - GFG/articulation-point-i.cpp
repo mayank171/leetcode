51e9ce41e09ba840e10a7d83853cae2c159b409a
mayank <46345959+mayank171@users.noreply.github.com>
2022-07-14 12:38:39 +0530

    Added solution - LeetHub

--- Articulation Point - I - GFG/articulation-point-i.cpp
+++ Articulation Point - I - GFG/articulation-point-i.cpp
@@ -10,47 +10,81 @@ using namespace std;
 class Solution {
   private:
     
-    void dfs(vector<int> adj[],vector<int> &disc,vector<int> &parent,vector<int> &low,vector<bool> &ap,int src,vector<int> &vis)
+    // void dfs(vector<int> adj[],vector<int> &disc,vector<int> &parent,vector<int> &low,vector<bool> &ap,int src,vector<int> &vis)
+    // {
+    //     static int time=0;
+    //     int count=0;
+    //     disc[src]=low[src]=time;
+    //     time++;
+    //     vis[src]=1;
+    //     //count++;
+        
+    //     for(auto &it:adj[src])
+    //     {
+    //         if(parent[src]==it)
+    //             continue;
+    //         else if(vis[it]==1)
+    //         {
+    //             low[src]=min(low[src],disc[it]);
+    //         }
+    //         else
+    //         {
+    //             parent[it]=src;
+    //             count++;
+    //             dfs(adj,disc,parent,low,ap,it,vis);
+                
+    //             low[src]=min(low[src],low[it]);
+                
+    //             if(parent[src]==-1)
+    //             {
+    //                 if(count>=2)
+    //                 {
+    //                     ap[src]=true;
+    //                 }
+    //             }
+    //             else 
+    //             {
+    //                 if(low[it]>=disc[src])
+    //                 {
+    //                     ap[src]=true;
+    //                 }
+    //             }
+    //         }
+            
+    //     }
+    // }
+    
+    
+    void dfs(vector<int> adj[],int V,vector<int> &disc,vector<int> &parent,vector<int> &low,int src,vector<int> &artiPoint)
     {
-        static int time=0;
-        int count=0;
-        disc[src]=low[src]=time;
-        time++;
-        vis[src]=1;
-        //count++;
+        static int t=0;
+        disc[src]=low[src]=t;
+        t++;
+        int childCt=0;
         
         for(auto &it:adj[src])
         {
-            if(parent[src]==it)
-                continue;
-            else if(vis[it]==1)
-            {
-                low[src]=min(low[src],disc[it]);
-            }
-            else
+            if(disc[it]==-1)
             {
                 parent[it]=src;
-                count++;
-                dfs(adj,disc,parent,low,ap,it,vis);
+                childCt++;
+                dfs(adj,V,disc,parent,low,it,artiPoint);
                 
                 low[src]=min(low[src],low[it]);
                 
-                if(parent[src]==-1)
+                if(parent[src]==-1 && childCt>1)
                 {
-                    if(count>=2)
-                    {
-                        ap[src]=true;
-                    }
+                    artiPoint[src]=1;
                 }
-                else 
+                else if(parent[src]!=-1 && low[it]>=disc[src])
                 {
-                    if(low[it]>=disc[src])
-                    {
-                        ap[src]=true;
-                    }
+                    artiPoint[src]=1;
                 }
             }
-            
+            else if(it!=parent[src])
+            {
+                low[src]=min(low[src],disc[it]);
+            }
         }
     }
     
@@ -58,33 +92,92 @@ class Solution {
   
     vector<int> articulationPoints(int V, vector<int>adj[]) {
         // Code here
+        
+        
+        
+        
+        
+        
         vector<int> disc(V,-1);
         vector<int> parent(V,-1);
-        vector<int> low(V,-1);
-        vector<bool> ap(V,false);
-        vector<int> vis(V,0);
-        
+        vector<int> low(V);
+        vector<int> artiPoint(V,0);
+        set<int> st;
         
         for(int i=0;i<V;i++)
         {
             if(disc[i]==-1)
             {
-                dfs(adj,disc,parent,low,ap,i,vis);
+                dfs(adj,V,disc,parent,low,i,artiPoint);
             }
         }
         
+        
+        
         vector<int> ans;
+        int flag=0;
         
         for(int i=0;i<V;i++)
         {
-            if(ap[i]==true)
-              ans.push_back(i);
+            if(artiPoint[i]==1)
+            {
+                flag=1;
+               ans.push_back(i); 
+            }
+               
         }
         
-        if(ans.size()>0)
+        if(flag==1)
         return ans;
-        
+        else
         return {-1};
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // vector<int> disc(V,-1);
+        // vector<int> parent(V,-1);
+        // vector<int> low(V,-1);
+        // vector<bool> ap(V,false);
+        // vector<int> vis(V,0);
+        
+        
+        // for(int i=0;i<V;i++)
+        // {
+        //     if(disc[i]==-1)
+        //     {
+        //         dfs(adj,disc,parent,low,ap,i,vis);
+        //     }
+        // }
+        
+        // vector<int> ans;
+        
+        // for(int i=0;i<V;i++)
+        // {
+        //     if(ap[i]==true)
+        //       ans.push_back(i);
+        // }
+        
+        // if(ans.size()>0)
+        // return ans;
+        
+        // return {-1};
     }
 };
 
