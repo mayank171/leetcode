c895d3b7ffcb09d6404a44fe9b055b5bebba3d86
mayank <46345959+mayank171@users.noreply.github.com>
2022-07-21 10:47:48 +0530

    Time: 18 ms (20.53%), Space: 13.2 MB (21.67%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -4,9 +4,42 @@ public:
         
         int n=prices.size();
         
-        vector<vector<int>> dp(n+1,vector<int>(2,0));
+//         vector<vector<int>> dp(n+1,vector<int>(2,0));
         
-        dp[n][0]=dp[n][1]=0;
+//         dp[n][0]=dp[n][1]=0;
+        
+//         for(int i=n-1;i>=0;i--)
+//         {
+//             for(int j=0;j<2;j++)
+//             {
+//                 int profit=0;
+//                 if(j)
+//                 {
+//                     int a=-prices[i]+dp[i+1][0];
+//                     int b=0+dp[i+1][1];
+//                     if(a>b)
+//                         profit=a;
+//                     else
+//                         profit=b;
+//                 }
+//                 else
+//                 {
+//                     int a=prices[i]+dp[i+1][1];
+//                     int b=0+dp[i+1][0];
+//                     if(a>b)
+//                         profit=a;
+//                     else
+//                         profit=b;
+//                 }
+//                 dp[i][j]=profit;
+//             }
+//         }
+        
+//         return dp[0][1];
+        
+        vector<int> dp(n+1,0);
+        int ahead=0;
+        int back=0;
         
         for(int i=n-1;i>=0;i--)
         {
@@ -15,27 +48,20 @@ public:
                 int profit=0;
                 if(j)
                 {
-                    int a=-prices[i]+dp[i+1][0];
-                    int b=0+dp[i+1][1];
-                    if(a>b)
-                        profit=a;
-                    else
-                        profit=b;
+                    profit=max(-1*prices[i]+ahead,0+back);
                 }
                 else
                 {
-                    int a=prices[i]+dp[i+1][1];
-                    int b=0+dp[i+1][0];
-                    if(a>b)
-                        profit=a;
-                    else
-                        profit=b;
+                    profit=max(prices[i]+back,ahead);
                 }
-                dp[i][j]=profit;
+                
+                if(j==0)
+                    ahead=profit;
+                else
+                    back=profit;
             }
         }
-        
-        return dp[0][1];
-        
+        cout<<ahead<<" "<<back<<endl;
+        return  back;
     }
 };
