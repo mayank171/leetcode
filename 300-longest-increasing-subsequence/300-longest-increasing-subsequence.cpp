75112bcf4dbcd0e34d0681949f2101602b71bae0
mayank <46345959+mayank171@users.noreply.github.com>
2022-07-27 09:33:42 +0530

    Time: 11 ms (89.66%), Space: 10.5 MB (44.40%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -2,35 +2,32 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         
-       int n=nums.size();
+        int n=nums.size();
         
         vector<int> ans;
-        ans.push_back(nums[0]);
         
-        for(int i=1;i<n;i++)
+        for(int i=0;i<n;i++)
         {
-            int sz=ans.size();
-            if(nums[i]>ans[sz-1])
+            int size=ans.size();
+            
+            if(size==0)
             {
                 ans.push_back(nums[i]);
             }
             else
             {
-                int ind=upper_bound(ans.begin(),ans.end(),nums[i])-ans.begin();
-                if(ind==sz)
+                int ind=lower_bound(ans.begin(),ans.end(),nums[i])-ans.begin();
+               // cout<<ind<<endl;
+                if(ind==size)
+                {
+                    ans.push_back(nums[i]);
+                }
+                else if(ind>0 && ans[ind-1]==nums[i])
                     continue;
-                else if(ind-1>=0 && ans[ind-1]==nums[i])
-                    continue;
-                else
+                else 
                     ans[ind]=nums[i];
             }
         }
-        
-        for(auto &it:ans)
-        {
-            cout<<it<<" ";
-        }
-        cout<<endl;
         
         return ans.size();
         
