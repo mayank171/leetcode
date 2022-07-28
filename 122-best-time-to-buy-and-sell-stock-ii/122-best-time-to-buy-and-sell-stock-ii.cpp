0bf93b9cdd3d7d5f05596bfd0923f8313a49c432
mayank <46345959+mayank171@users.noreply.github.com>
2022-07-28 09:37:44 +0530

    Time: 6 ms (85.52%), Space: 13.3 MB (20.56%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -4,6 +4,35 @@ public:
         
         int n=prices.size();
         
+        vector<int> vp;
+        
+        for(int i=1;i<n;i++)
+        {
+            vp.push_back(prices[i]-prices[i-1]);
+        }
+        
+        int size=vp.size();
+        
+        int ans=0;
+        int cur=0;
+        for(int i=0;i<size;i++)
+        {
+            if(vp[i]<0)
+            {
+                ans+=cur;
+                cur=0;
+            }
+            else
+            {
+                cur+=vp[i];
+            }
+        }
+        if(cur>0)
+            ans+=cur;
+        
+        
+        return ans;
+        
 //         vector<vector<int>> dp(n+1,vector<int>(2,0));
         
 //         dp[n][0]=dp[n][1]=0;
@@ -35,33 +64,40 @@ public:
 //             }
 //         }
         
+//         for(auto &it:dp)
+//         {
+//             for(auto &it1:it)
+//                 cout<<it1<<" ";
+//             cout<<endl;
+//         }
+        
 //         return dp[0][1];
         
-        vector<int> dp(n+1,0);
-        int ahead=0;
-        int back=0;
+//         vector<int> dp(n+1,0);
+//         int ahead=0;
+//         int back=0;
         
-        for(int i=n-1;i>=0;i--)
-        {
-            for(int j=0;j<2;j++)
-            {
-                int profit=0;
-                if(j)
-                {
-                    profit=max(-1*prices[i]+ahead,0+back);
-                }
-                else
-                {
-                    profit=max(prices[i]+back,ahead);
-                }
+//         for(int i=n-1;i>=0;i--)
+//         {
+//             for(int j=0;j<2;j++)
+//             {
+//                 int profit=0;
+//                 if(j)
+//                 {
+//                     profit=max(-1*prices[i]+ahead,0+back);
+//                 }
+//                 else
+//                 {
+//                     profit=max(prices[i]+back,ahead);
+//                 }
                 
-                if(j==0)
-                    ahead=profit;
-                else
-                    back=profit;
-            }
-        }
-        cout<<ahead<<" "<<back<<endl;
-        return  back;
+//                 if(j==0)
+//                     ahead=profit;
+//                 else
+//                     back=profit;
+//             }
+//         }
+    
+//         return  back;
     }
 };
