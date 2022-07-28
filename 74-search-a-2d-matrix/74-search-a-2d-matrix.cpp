db16b8de0e220d53da829885f1195672cc52b9dc
mayank <46345959+mayank171@users.noreply.github.com>
2022-07-28 16:18:49 +0530

    Time: 6 ms (60.03%), Space: 9.5 MB (51.72%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -2,27 +2,54 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& mat, int target) {
         
-        int row=-1;
+//         int row=-1;
+//         int n=mat.size();
+//         int m=mat[0].size();
+        
+//         for(int i=0;i<n;i++)
+//         {
+//             if(mat[i][0]<=target)
+//                 row=i;
+//         }
+        
+//         if(row==-1)
+//             return false;
+        
+//         int ind=lower_bound(mat[row].begin(),mat[row].end(),target)-mat[row].begin();
+        
+//         if(ind==m)
+//             return false;
+//         if(mat[row][ind]==target)
+//             return true;
+//         else
+//             return false;
+        
         int n=mat.size();
         int m=mat[0].size();
         
-        for(int i=0;i<n;i++)
+        int low=0;
+        int high=(n-1)*m+(m-1);
+        
+        while(low<=high)
         {
-            if(mat[i][0]<=target)
-                row=i;
+            int mid=(low+high)/2;
+            
+            if(mat[mid/m][mid%m]==target)
+            {
+                return true;
+            }
+            else if(mat[mid/m][mid%m]>target)
+            {
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
         }
         
-        if(row==-1)
-            return false;
+        return false;
         
-        int ind=lower_bound(mat[row].begin(),mat[row].end(),target)-mat[row].begin();
-        
-        if(ind==m)
-            return false;
-        if(mat[row][ind]==target)
-            return true;
-        else
-            return false;
     
         
     }
