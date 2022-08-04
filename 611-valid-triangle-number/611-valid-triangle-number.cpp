8ba11d6cac439bb16513c6aab190ec9cffe3d42a
mayank <46345959+mayank171@users.noreply.github.com>
2022-08-04 17:26:45 +0530

    Time: 182 ms (49.22%), Space: 13 MB (53.94%) - LeetHub

--- 611-valid-triangle-number/611-valid-triangle-number.cpp
+++ 611-valid-triangle-number/611-valid-triangle-number.cpp
@@ -5,30 +5,27 @@ public:
         int n=nums.size();
         
         sort(nums.begin(),nums.end());
-        
         int ans=0;
         
         for(int i=2;i<n;i++)
         {
-            int j=0;
-            int k=i-1;
-            
-            while(j<k)
+            int l=0;
+            int r=i-1;
+            while(l<r)
             {
-                if(nums[j]+nums[k]>nums[i])
+                if(nums[l]+nums[r]>nums[i])
                 {
-                    ans+=k-j;
-                    k--;
-                    //break;
+                    ans+=(r-l);
+                    r--;
                 }
                 else
                 {
-                    j++;
+                    l++;
                 }
             }
-            
         }
         
         return ans;
+        
     }
 };
