3041ae6fbea0ffd26dd2c826c3ba12217c4cfebc
mayank <46345959+mayank171@users.noreply.github.com>
2022-08-07 18:14:53 +0530

    Time: 93 ms (62.13%), Space: 18.2 MB (11.31%) - LeetHub

--- 221-maximal-square/221-maximal-square.cpp
+++ 221-maximal-square/221-maximal-square.cpp
@@ -19,10 +19,6 @@ public:
             {
                 if(dp[i][j]==0)
                     continue;
-                else if((dp[i-1][j]==0 || dp[i][j-1]==0 || dp[i-1][j-1]==0) && dp[i][j]==1)
-                {
-                    dp[i][j]=1;
-                }
                 else 
                 {
                     dp[i][j]=min(dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1]))+1;
