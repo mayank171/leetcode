ce98956aaf3f6279af89da7fd5ef2545a74155e4
mayank <46345959+mayank171@users.noreply.github.com>
2022-08-15 13:41:31 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (75.16%) - LeetHub

--- 769-max-chunks-to-make-sorted/769-max-chunks-to-make-sorted.cpp
+++ 769-max-chunks-to-make-sorted/769-max-chunks-to-make-sorted.cpp
@@ -4,13 +4,12 @@ public:
         
         int n=arr.size();
         
-        int maxi=0;
         int ct=0;
+        int maxi=-1;
         for(int i=0;i<n;i++)
         {
             maxi=max(maxi,arr[i]);
-            
-            if(maxi==i)
+            if(i==maxi)
                 ct++;
         }
         
