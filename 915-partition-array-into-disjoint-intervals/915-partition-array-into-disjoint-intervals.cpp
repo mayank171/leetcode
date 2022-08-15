e40ea2095d6b9d47c11af4126e2c707baa43582f
mayank <46345959+mayank171@users.noreply.github.com>
2022-08-15 14:03:33 +0530

    Time: 230 ms (64.05%), Space: 102.4 MB (14.51%) - LeetHub

--- 915-partition-array-into-disjoint-intervals/915-partition-array-into-disjoint-intervals.cpp
+++ 915-partition-array-into-disjoint-intervals/915-partition-array-into-disjoint-intervals.cpp
@@ -4,49 +4,45 @@ public:
         
         int n=nums.size();
         
-        vector<int> v1;
-        v1.push_back(nums[0]);
+        vector<int> maxLeft(n+1);
+        maxLeft[0]=nums[0];
         
         for(int i=1;i<n;i++)
         {
-            v1.push_back(max(v1[i-1],nums[i]));
+            maxLeft[i]=max(nums[i],maxLeft[i-1]);
         }
         
-        vector<int> v2(n);
-        v2[n-1]=nums[n-1];
+        vector<int> minRight(n+1);
+        minRight[n]=1e8;
         
-        for(int i=n-2;i>=0;i--)
+        for(int i=n-1;i>=0;i--)
         {
-            v2[i]=min(v2[i+1],nums[i]);
+            minRight[i]=min(nums[i],minRight[i+1]);
         }
         
+        int ctr=0;
         
-//         for(auto &it:v1)
-//         {
+//         for(auto &it:maxLeft)
 //             cout<<it<<" ";
-//         }
 //         cout<<endl;
         
-//         for(auto &it:v2)
-//         {
+//         for(auto &it:minRight)
 //             cout<<it<<" ";
-//         }
 //         cout<<endl;
         
         
-        
-        int ans=n;
-        for(int i=n-1;i>0;i--)
+        ctr=1;
+        for(int i=0;i<n-1;i++)
         {
-            if(v1[i-1]<=v2[i])
+            if(maxLeft[i]>minRight[i+1])
             {
-                ans=i;
+                ctr++;
             }
+            else
+                break;
         }
         
-        return ans;
-        
-        
+        return ctr;
         
     }
 };
