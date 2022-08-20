7e37c7c89b38c6686dbe72a8d34cd553722d06fa
mayank <46345959+mayank171@users.noreply.github.com>
2022-08-20 15:53:31 +0530

    Time: 19 ms (5.87%), Space: 11.9 MB (93.26%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -2,45 +2,51 @@ class Solution {
 public:
     void nextPermutation(vector<int>& arr) {
         
-        int index=-1;
+        int flag=0;
+        int n=arr.size();
         
-        int N=arr.size();
-        
-        for(int i=1;i<N;i++)
+        for(int i=0;i<n-1;i++)
         {
-            if(arr[i-1]<arr[i])
+            if(arr[i]<arr[i+1])
             {
+                flag=1;
+                break;
+            }
+        }
+        
+        if(flag==0)
+        {
+            sort(arr.begin(),arr.end());
+            return ;
+        }
+        
+        int ind=n-1;
+        
+        for(int i=n-1;i>0;i--)
+        {
+            if(arr[i]>arr[i-1])
+            {
+                ind=i-1;
+                break;
+            }
+        }
+        
+        int min_diff=1e8;
+        int index=ind+1;
+        for(int i=ind+1;i<n;i++)
+        {
+            if(arr[i]>arr[ind] && arr[i]-arr[ind]<min_diff)
+            {
+                min_diff=arr[i]-arr[ind];
                 index=i;
             }
         }
         
-        if(index==-1)
-        {
-            reverse(arr.begin(),arr.end());
-            return ;
-        }
+        swap(arr[ind],arr[index]);
         
-        int lastInd=-1;
-        for(int i=index+1;i<N;i++)
-        {
-            if(arr[index-1]<arr[i])
-            {
-                lastInd=i;
-            }
-        }
+        sort(arr.begin()+ind+1,arr.end());
         
-        if(lastInd==-1)
-        {
-            swap(arr[index],arr[index-1]);
-            reverse(arr.begin()+index,arr.end());
-            return ;
-        }
-        else
-        {
-            swap(arr[index-1],arr[lastInd]);
-            reverse(arr.begin()+index,arr.end());    
-        }
-        
+        return;
         
     }
 };
