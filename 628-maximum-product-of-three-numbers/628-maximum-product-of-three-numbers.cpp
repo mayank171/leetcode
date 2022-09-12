9d75a96bb75a733310a8f7af0665285ab5e64f2c
mayank <46345959+mayank171@users.noreply.github.com>
2022-09-12 14:18:51 +0530

    Time: 121 ms (5.47%), Space: 27.7 MB (36.25%) - LeetHub

--- 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.cpp
+++ 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.cpp
@@ -2,70 +2,25 @@ class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
         
-        
         int n=nums.size();
         
-//         sort(nums.begin(),nums.end());
+        sort(nums.begin(),nums.end());
         
-//         return nums[n-1]*nums[n-2]*nums[n-3];
+        int maxi1=nums[n-1];
+        int maxi2=nums[n-2];
+        int maxi3=nums[n-3];
         
-        int max1=-1e8;
-        int max2=-1e8;
-        int max3=-1e8;
-        int min1=1e8;
-        int min2=1e8;
-        int min3=1e8;
-        
-        
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]>max1)
-            {
-                max3=max2;
-                max2=max1;
-                max1=nums[i];
-            }
-            else if(nums[i]>max2)
-            {
-                max3=max2;
-                max2=nums[i];
-            }
-            else if(nums[i]>max3)
-            {
-                max3=nums[i];
-            }
-            
-            if(nums[i]<min1)
-            {
-                min3=min2;
-                min2=min1;
-                min1=nums[i];
-            }
-            else if(nums[i]<min2)
-            {
-                min3=min2;
-                min2=nums[i];
-            }
-            else if(nums[i]<min3)
-            {
-                min3=nums[i];
-            }
-            
-            
-        }
-        
-        cout<<max1<<" "<<max2<<" "<<max3<<endl;
-        cout<<min1<<" "<<min2<<" "<<min3<<endl;
-        
-        vector<int> prod;
-        prod.push_back(max1*max2*max3);
-        prod.push_back(min1*min2*min3);
-        prod.push_back(min1*min2*max1);
+        int mini1=nums[0];
+        int mini2=nums[1];
+        int mini3=nums[2];
         
         
-        return *max_element(prod.begin(),prod.end());
+        vector<int> temp;
+        temp.push_back(maxi1*maxi2*maxi3);
+        temp.push_back(maxi1*mini1*mini2);
+        temp.push_back(mini1*mini2*mini3);
         
         
-        
+        return *max_element(temp.begin(),temp.end());
     }
 };
