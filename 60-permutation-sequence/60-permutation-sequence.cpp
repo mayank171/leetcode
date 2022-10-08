5f66afb0fa71387208150f4edaca304e06be58c7
mayank <46345959+mayank171@users.noreply.github.com>
2022-10-08 21:29:39 +0530

    Time: 7 ms (32.91%), Space: 6.1 MB (16.58%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.cpp
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -1,93 +1,35 @@
 class Solution {
 private:
     
-//     bool check(vector<char> v ,int n,string &ans,int &k)
-//     {
-//         if(ans.length()==n)
-//         {
-//             k--;
-//             if(k==0)
-//               return true;
-//             else
-//               return false;
-//         }
-        
-        
-//         for(int i=0;i<n;i++)
-//         {
-            
-//             char ch;
-//             if(v[i]!='/')
-//                 ch=v[i];
-//             else
-//                 continue;
-//             ans.push_back(ch);
-//             v[i]='/';
-//             if(check(v,n,ans,k))
-//                 return true;
-//             else
-//             {
-//                 v[i]=ch;
-//                 ans.pop_back();
-//             }
-                
-        
-//         }
-//         return false;
-//     }
-    
     
 public:
     string getPermutation(int n, int k) {
         
-//         string ans="";
-        
-//         int ind=0;
-        
-//         vector<char> v;
-        
-//         for(int i=1;i<=n;i++)
-//         {
-//             char ch=i+'0';
-//             v.push_back(ch);
-//         }
-   
-//         if(check(v,n,ans,k))
-//             return ans;
-//         else
-//             return ans;
-        
-        
-        vector<char> v;
-        
-        for(int i=1;i<=n;i++)
-        {
-            char ch=i+'0';
-            v.push_back(ch);
-        }
-        
-        int fact=1;
-        int c=1;
-        
-        while(c<n)
-        {
-            fact*=c;
-            c++;
-        }
         
         string ans="";
+        
+        vector<int> nums;
+        int fact=1;
+        
+        for(int i=1;i<n;i++)
+        {
+            nums.push_back(i);
+            fact=fact*i;
+        }
+        
+        nums.push_back(n);
+        
         k--;
         
-        while(v.size())
+        while(true)
         {
-            int x=k/fact;
+            ans=ans+to_string(nums[k/fact]);
+            nums.erase(nums.begin()+k/fact);
+        
+            if(nums.size()==0)
+                break;
             k=k%fact;
-            
-            ans+=v[x];
-            v.erase(v.begin()+x);
-            
-            if(v.size()!=0)
-            fact=fact/v.size();
+            fact=fact/nums.size();
         }
         
         return ans;
