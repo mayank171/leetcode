14d0d7015b8a1d603e1de6497fa95a094a9c35d0
mayank <46345959+mayank171@users.noreply.github.com>
2022-10-25 23:25:39 +0530

    Time: 174 ms (31.46%), Space: 53.2 MB (11.56%) - LeetHub

--- 0795-number-of-subarrays-with-bounded-maximum/0795-number-of-subarrays-with-bounded-maximum.cpp
+++ 0795-number-of-subarrays-with-bounded-maximum/0795-number-of-subarrays-with-bounded-maximum.cpp
@@ -5,49 +5,52 @@ public:
         int n=nums.size();
         
         int ans=0;
-        
         int ctless=0;
         int ct=0;
         
-        vector<int> v;
+        vector<int> vp;
         
         for(int i=0;i<n;i++)
         {
             if(nums[i]<left)
             {
-                ctless++;
                 ct++;
+                ctless++;
             }
             else if(nums[i]>=left && nums[i]<=right)
             {
                 if(ctless>0)
                 {
-                    v.push_back(ctless);
+                    vp.push_back(ctless);
                     ctless=0;
                 }
                 ct++;
+               
             }
             else if(nums[i]>right)
             {
-                ans+=(ct*1ll*(ct+1))/2;
                 if(ctless>0)
-                   v.push_back(ctless);
-                ctless=0;
+                {
+                    vp.push_back(ctless);
+                    ctless=0;
+                }
+                ans+=(ct*1ll*(ct+1))/2;
                 ct=0;
             }
-                
         }
         
-        if(ctless>0)
-            v.push_back(ctless);
-        
         if(ct>0)
-            ans+=(ct*1ll*(ct+1))/2;
-        
-        int size=v.size();
-        for(int i=0;i<size;i++)
         {
-            ans-=(v[i]*1ll*(v[i]+1))/2;
+            ans+=(ct*1ll*(ct+1))/2;    
+        }
+        if(ctless>0)
+        {
+            vp.push_back(ctless);
+        }
+        
+        for(int i=0;i<vp.size();i++)
+        {
+            ans-=(vp[i]*1ll*(vp[i]+1))/2;
         }
         
         return ans;
