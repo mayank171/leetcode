13df9eaec39431b260e9974ebe389610916a1cb1
mayank <46345959+mayank171@users.noreply.github.com>
2022-10-30 15:07:02 +0530

    Time: 2 ms (94.74%), Space: 6 MB (36.84%) - LeetHub

--- 2457-minimum-addition-to-make-integer-beautiful/2457-minimum-addition-to-make-integer-beautiful.cpp
+++ 2457-minimum-addition-to-make-integer-beautiful/2457-minimum-addition-to-make-integer-beautiful.cpp
@@ -1,80 +1,30 @@
 class Solution {
 public:
     
-    bool solve(long long n,int target)
+    long long solve(long long n)
     {
         long long sum=0;
-        long long ct=0;
         while(n)
         {
             sum+=n%10;
             n=n/10;
-            ct++;
         }
         
-        if(sum<=target)
-            return true;
-        return false;
+        return sum;
     }
     
     long long makeIntegerBeautiful(long long n, int target) {
         
-        long long sum=0;
+        long long int p=10;
+        long long m=n;
         
-        long long nn=n;
-        
-        long long ct=0;
-        
-        while(nn)
+        while(solve(n)>target)
         {
-            sum+=nn%10;
-            nn=nn/10;
-            ct++;
+            n=ceil(n/(double)p)*p;
+            p=p*10;
         }
         
-        if(sum<=target)
-            return 0;
+        return n-m;
         
-        long long res=0;
-        string temp="";
-        for(int i=ct-1;i>=0;i--)
-        {
-            if(solve(n,target))
-                break;
-            
-            long long x=n%10;
-            x=10-x;
-            
-            if(x!=10)
-            {
-                res=10*res+x;
-                n=n/10+1;
-                temp+=(x+'0');
-            }
-            else
-            {
-                res*=10;
-                n=n/10;
-                temp+='0';
-            }   
-            
-            cout<<x<<" "<<n<<" "<<res<<endl;
-        }
-        
-        cout<<res<<" "<<temp<<endl;
-        
-        reverse(temp.begin(),temp.end());
-        
-        long long k=0;
-        int l=temp.length();
-        int j=0;
-        
-        while(j<l)
-        {
-            k=k*10+(temp[j]-'0');
-            j++;
-        }
-        
-        return k;
     }
 };
