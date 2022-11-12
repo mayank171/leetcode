acf62adfc7fb5ac2b3c08da93335bdba19af531c
mayank <46345959+mayank171@users.noreply.github.com>
2022-11-12 15:23:44 +0530

    Added solution - LeetHub

--- Maximum of all subarrays of size k - GFG/maximum-of-all-subarrays-of-size-k.cpp
+++ Maximum of all subarrays of size k - GFG/maximum-of-all-subarrays-of-size-k.cpp
@@ -50,25 +50,114 @@ class Solution
     
     vector <int> max_of_subarrays(int *arr, int n, int k)
     {
-        vector<int> seg(4*n,-1);
-        buildTree(arr,seg,0,n-1,0);
+    //     vector<int> seg(4*n,-1);
+    //     buildTree(arr,seg,0,n-1,0);
         
-       // return {};
+    //   // return {};
         
-        vector<int> ans;
+    //     vector<int> ans;
         
-        for(int i=0;i<=n-k;i++)
-        {
-            int qs=i;
-            int qe=i+k-1;
+    //     for(int i=0;i<=n-k;i++)
+    //     {
+    //         int qs=i;
+    //         int qe=i+k-1;
             
-            ans.push_back(query(arr,seg,0,n-1,0,qs,qe));
-            //break;
-        }
+    //         ans.push_back(query(arr,seg,0,n-1,0,qs,qe));
+    //         //break;
+    //     }
         
-        return ans;
+    //     return ans;
+    
+     
+       vector<int> nse(n,n);
+       
+       stack<int> stk;
+       
+       for(int i=0;i<n;i++)
+       {
+           if(!stk.empty())
+           {
+               if(arr[stk.top()]<arr[i])
+               {
+                   while(!stk.empty() && arr[stk.top()]<arr[i])
+                   {
+                       nse[stk.top()]=i;
+                       stk.pop();
+                   }
+                   stk.push(i);    
+               }
+               else
+               {
+                   stk.push(i);
+               }
+           }
+           else
+           {
+               stk.push(i);
+           }
+       }
+       
+    //   for(auto &it:nse)
+    //   {
+    //       cout<<arr[it]<<" ";
+    //   }
+    //   cout<<endl;
+       
+       vector<int> ans(n-k+1);
+       
+       int i=0;
+       int j=0;
+       
+       while(i<=n-k)
+       {
+           if(j<i)
+           {
+               j=i;
+           }
+           
+           while(nse[j]<=(i+k-1))
+           {
+               j=nse[j];
+           }
+           
+           ans[i]=arr[j];
+           i++;
+       }
+       
+       return ans;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //{ Driver Code Starts.
 
