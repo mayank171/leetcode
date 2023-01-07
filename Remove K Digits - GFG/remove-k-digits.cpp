4629c69f27fb3160fb24d41442b48a6d4b4f8ccf
mayank <46345959+mayank171@users.noreply.github.com>
2023-01-07 18:35:29 +0530

    Added solution - LeetHub

--- Remove K Digits - GFG/remove-k-digits.cpp
+++ Remove K Digits - GFG/remove-k-digits.cpp
@@ -9,58 +9,71 @@ using namespace std;
 
 class Solution {
   public:
-    string removeKdigits(string num, int k) {
+    string removeKdigits(string s, int k) {
         
-        int n=num.length();
-
-
-          stack<char> stk;
-
-          for(int i=0;i<n;i++)
-          {
-              if(stk.empty())
-              {
-                  stk.push(num[i]);
-              }
-              else
-              {
-                  while(!stk.empty() && k>0 && stk.top()>num[i])
-                  {
-                      stk.pop();
-                      k--;
-                  }
-                  stk.push(num[i]);
-              }
-          }
-
-          string ans="";
-
-          while(!stk.empty())
-          {
-              ans.push_back(stk.top());
-              stk.pop();
-          }
-
-          while(ans.size()>0 && ans.back()=='0')
-          {
-              ans.pop_back();
-          }
-
-          reverse(ans.begin(),ans.end());
-
-          while(k && ans.size()>0)
-          {
-              ans.pop_back();
-              k--;
-          }
-
-          if(ans.size()==0)
-             return "0";
-
-          return ans;
+        int n=s.length();
+        
+        stack<char> stk;
+        
+        for(int i=0;i<n;i++)
+        {
+            if(stk.empty())
+            {
+                stk.push(s[i]);
+            }
+            else
+            {
+                while(!stk.empty() && stk.top()>s[i] && k>0)
+                {
+                    stk.pop();
+                    k--;
+                }
+                
+                stk.push(s[i]);
+            }
+        }
+        
+        string ans="";
+        while(!stk.empty())
+        {
+            ans+=stk.top();
+            stk.pop();
+        }
+        
+        while(ans.size()>0 && ans.back()=='0')
+        {
+            ans.pop_back();
+        }
+        
+        reverse(ans.begin(),ans.end());
+        
+        while(k && ans.size()>0)
+        {
+            ans.pop_back();
+            k--;
+        }
+        
+        if(ans.size()==0)
+           return "0";
+        
+        return ans;
         
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //{ Driver Code Starts.
 
