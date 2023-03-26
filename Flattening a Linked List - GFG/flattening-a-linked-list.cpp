12c9a9717527c614315984d86fb777cf3390e586
mayank <46345959+mayank171@users.noreply.github.com>
2023-03-26 21:38:02 +0530

    Added solution - LeetHub

---
+++ Flattening a Linked List - GFG/flattening-a-linked-list.cpp
@@ -0,0 +1,160 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+struct Node{
+	int data;
+	struct Node * next;
+	struct Node * bottom;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	    bottom = NULL;
+	}
+	
+};
+
+using namespace std;
+
+void printList(Node *Node)
+{
+    while (Node != NULL)
+    {
+        printf("%d ", Node->data);
+        Node = Node->bottom;
+    }
+}
+
+Node* flatten (Node* root);
+
+int main(void) {
+
+	int t;
+	cin>>t;
+	while(t--){
+    int n,m,flag=1,flag1=1;
+    struct Node * temp=NULL;
+	struct Node * head=NULL;
+	struct Node * pre=NULL;
+	struct Node * tempB=NULL;
+	struct Node * preB=NULL;
+		cin>>n;
+        int work[n];
+		for(int i=0;i<n;i++)
+            cin>>work[i];
+		for(int i=0;i<n;i++){
+			m=work[i];
+			--m;
+			int data;
+			scanf("%d",&data);
+			temp = new Node(data);
+			temp->next = NULL;
+			temp->bottom = NULL;
+			
+			if(flag){
+				head = temp;
+				pre = temp;
+				flag = 0;
+				flag1 = 1;
+			}
+			else{
+				pre->next = temp;
+				pre = temp;
+				flag1 = 1;
+			}
+			for(int j=0;j<m;j++){
+				
+				int temp_data;
+				scanf("%d",&temp_data);
+				tempB = new Node(temp_data);
+
+				if(flag1){
+					temp->bottom=tempB;
+					preB=tempB;
+					flag1=0;
+				}
+				else{
+					preB->bottom=tempB;
+					preB=tempB;
+				}
+			}
+		}
+		   Node *fun = head;
+		   Node *fun2=head;
+
+            Node* root = flatten(head);
+            printList(root);
+            cout<<endl;
+
+	}
+	return 0;
+}
+
+// } Driver Code Ends
+
+
+/* Node structure  used in the program
+
+struct Node{
+	int data;
+	struct Node * next;
+	struct Node * bottom;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	    bottom = NULL;
+	}
+	
+};
+*/
+
+Node* solve(Node* root,Node* node)
+{
+    Node* temp=new Node(0);
+    
+    Node* x=temp;
+    
+    while(root && node)
+    {
+        if(root->data<node->data)
+        {
+            x->bottom=root;
+            x=x->bottom;
+            root=root->bottom;
+        }
+        else
+        {
+            x->bottom=node;
+            x=x->bottom;
+            node=node->bottom;
+        }
+    }
+    
+    while(root)
+    {
+        x->bottom=root;
+        root=root->bottom;
+        x=x->bottom;
+    }
+    
+    while(node)
+    {
+        x->bottom=node;
+        node=node->bottom;
+        x=x->bottom;
+    }
+    
+    return temp->bottom;
+}
+
+Node *flatten(Node *root)
+{
+   Node* node;
+   if(root->next!=NULL)
+      node=flatten(root->next);
+   else
+      return root;
+   return solve(root,node);
+}
+
