81a332bbda3b0bcab36f37611b2e1b931bc47875
mayank <46345959+mayank171@users.noreply.github.com>
2023-04-15 10:52:14 +0530

    Time: 651 ms (10.21%), Space: 390.6 MB (5.24%) - LeetHub

--- 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
@@ -1,47 +1,35 @@
 class Solution {
 public:
     
-    int solve(vector<vector<int>> &vp,int n,int ind,int k,vector<vector<int>> &dp)
+    int solve(vector<vector<int>> &piles,int n,int k,int ind,vector<vector<int>> &dp)
     {
-        if(k<=0)
-            return 0;
-        
-        if(ind>=n)
+        if(ind>=n || k==0)
             return 0;
         
         if(dp[ind][k]!=-1)
             return dp[ind][k];
-    
-        int sum=0;
-        int sum1=solve(vp,n,ind+1,k,dp);
-        int cur=0;
-        for(int j=0;j<vp[ind].size() && j<k;j++)
+        
+        int sum1=solve(piles,n,k,ind+1,dp);
+        
+        int sum2=0;
+        int ans=0;
+        
+        for(int i=0;i<piles[ind].size() && i<k;i++)
         {
-            cur+=vp[ind][j];
-            sum=max(sum,solve(vp,n,ind+1,k-j-1,dp)+cur);
+            ans+=piles[ind][i];
+            sum2=max(sum2,ans+solve(piles,n,k-i-1,ind+1,dp));
         }
         
-        return dp[ind][k]=max(sum1,sum);
-    
+        return dp[ind][k]=max(sum1,sum2);
     }
     
     int maxValueOfCoins(vector<vector<int>>& piles, int k) {
         
         int n=piles.size();
-        vector<stack<int>> vp;
         
         vector<vector<int>> dp(1001,vector<int>(2001,-1));
-        for(int i=0;i<n;i++)
-        {
-            stack<int> ste;
-            for(int j=piles[i].size()-1;j>=0;j--)
-            {
-                ste.push(piles[i][j]);
-            }
-            vp.push_back(ste);
-        }
+            
+        return solve(piles,n,k,0,dp);
         
-        //return 0;
-        return solve(piles,n,0,k,dp);
     }
 };
