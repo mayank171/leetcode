c3e6ab84bd3ca36147957fa9addd45b7a65251a9
mayank <46345959+mayank171@users.noreply.github.com>
2023-05-03 08:48:21 +0530

    Added solution - LeetHub

--- Count Palindromic Subsequences - GFG/count-palindromic-subsequences.cpp
+++ Count Palindromic Subsequences - GFG/count-palindromic-subsequences.cpp
@@ -6,54 +6,54 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
     public:
-    /*You are required to complete below method */
     long long int  countPS(string str)
     {
+       int mod=1e9+7;
        int n=str.length();
        
-       int mod=1e9+7;
-       
-       vector<vector<long long int>> dp(n+1,vector<long long int> (n+1,0));
+       vector<vector<int>> dp(n,vector<int>(n,0));
        
        for(int gap=0;gap<n;gap++)
        {
            for(int i=0,j=gap;j<n;i++,j++)
            {
+            //   cout<<i<<" "<<j<<endl;
                if(gap==0)
                {
                    dp[i][j]=1;
                }
                else if(gap==1)
                {
-                    if(str[i]==str[j])
-                       dp[i][j]=3;
-                    else
-                       dp[i][j]=2;
+                   if(str[i]==str[j])
+                      dp[i][j]=3;
+                   else 
+                      dp[i][j]=2;
                }
                else
                {
-                    if(str[i]!=str[j])
-                    {
-                        long long int x=0;
-                        x=(x+dp[i][j-1])%mod;
-                        x=(x+dp[i+1][j])%mod;
-                        x=(x-dp[i+1][j-1]+mod)%mod;
-                        dp[i][j]=x;
-                    }
-                    else
-                    {
-                        long long int x=0;
-                        x=(x+dp[i][j-1])%mod;
-                        x=(x+dp[i+1][j])%mod;
-                        dp[i][j]=(x+1)%mod;
-                    }
+                   if(str[i]!=str[j])
+                   {
+                       dp[i][j]=((dp[i+1][j]+dp[i][j-1])%mod-dp[i+1][j-1]+mod)%mod;
+                   }
+                   else
+                   {
+                       dp[i][j]=((dp[i+1][j]+dp[i][j-1])%mod+1)%mod;
+                   }
                }
            }
        }
        
+    //   for(auto &it:dp)
+    //   {
+    //       for(auto &it1:it)
+    //           cout<<it1<<" ";
+    //       cout<<endl;
+    //   }
+       
+    //   cout<<"cdcd";
+       
        return dp[0][n-1];
     }
-     
 };
 
 //{ Driver Code Starts.
