a2b711ae4867f7b0cc3b6b747ff21e8d04b16632
mayank <46345959+mayank171@users.noreply.github.com>
2023-05-05 16:33:10 +0530

    Time: 6 ms (50.24%), Space: 8.2 MB (80.71%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
@@ -3,39 +3,58 @@ public:
     int sumOddLengthSubarrays(vector<int>& arr) {
         
         int n=arr.size();
+        int ans=0;
+        int p=0;
         
-        for(int i=1;i<n;i++)
+        for(int i=0;i<n;i++)
         {
-            arr[i]+=arr[i-1];
-        }
-        
-        for(auto &it:arr)
-        {
-            cout<<it<<" ";
-        }
-        cout<<endl;
- 
-        int sum=arr[n-1];
-        for(int i=2;i<n;i+=2)
-        {
-            for(int j=i;j<n;j++)
+            int right=n-i-p;
+            int left=i+1-p;
+            int total=right*left;
+            if(total&1)
             {
-            //    cout<<j<<" "<<i<<endl;
-                if(j-i>=0)
-                {
-                    cout<<j<<endl;
-                    cout<<j-i-1<<endl;
-                    int x=((j-i-1>=0)?arr[j-i-1]:0);
-                    //cout<<arr[j]<<" "<<x<<endl;
-                    sum+=(arr[j]-x);
-                   // cout<<"cdcdc"<<endl;
-                }
-                else
-                    break;
+                ans+=(arr[i]*((total/2)+1));
             }
+            else
+            {
+                ans+=(arr[i]*(total/2));
+            }
+            //p++;
         }
         
-        return sum;
+        return ans;
+//         for(int i=1;i<n;i++)
+//         {
+//             arr[i]+=arr[i-1];
+//         }
+        
+//         for(auto &it:arr)
+//         {
+//             cout<<it<<" ";
+//         }
+//         cout<<endl;
+ 
+//         int sum=arr[n-1];
+//         for(int i=2;i<n;i+=2)
+//         {
+//             for(int j=i;j<n;j++)
+//             {
+//             //    cout<<j<<" "<<i<<endl;
+//                 if(j-i>=0)
+//                 {
+//                     cout<<j<<endl;
+//                     cout<<j-i-1<<endl;
+//                     int x=((j-i-1>=0)?arr[j-i-1]:0);
+//                     //cout<<arr[j]<<" "<<x<<endl;
+//                     sum+=(arr[j]-x);
+//                    // cout<<"cdcdc"<<endl;
+//                 }
+//                 else
+//                     break;
+//             }
+//         }
+        
+//         return sum;
         
     }
 };
