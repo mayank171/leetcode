a8c449980b55ece602d39cf810aadc046b669a58
mayank <46345959+mayank171@users.noreply.github.com>
2023-05-05 10:32:18 +0530

    Time: 1096 ms (5.02%), Space: 77.7 MB (5.03%) - LeetHub

---
+++ 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    int minSubarray(vector<int>& nums, int p) {
+        
+        long long sum=0;
+        int n=nums.size();
+    
+        vector<long long> v;
+        for(int i=0;i<n;i++)
+        {
+            sum+=nums[i];   
+            v.push_back(nums[i]);
+        }
+    
+        
+        int rem=sum%p;
+        if(rem==0)
+            return 0;
+        if(p>sum)
+            return -1;
+        
+        map<int,int> mp;
+        mp[0]=-1;
+        int ans=1e9;
+        
+        for(int i=1;i<n;i++)
+        {
+            v[i]+=v[i-1];
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+            int r=v[i]%p;
+            cout<<r<<endl;
+            r=r-rem;
+            
+            if(r<0)
+            {
+                r+=p;
+            }
+            
+            cout<<"new"<<r<<endl;
+            
+            if(mp.find(r)!=mp.end())
+            {
+                if(i==n-1 && r==0 && mp[0]==-1)
+                {
+                    
+                }
+                else
+                {
+                    ans=min(ans,i-mp[r]);
+                    cout<<ans<<"ans"<<endl;        
+                }
+            }
+            
+            mp[v[i]%p]=i;
+        }
+        
+        if(ans==1e9)
+            return -1;
+        return ans;
+        
+    }
+};
