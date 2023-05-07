aa0e236b48576a70cd9c06c0173a6d23cd819879
mayank <46345959+mayank171@users.noreply.github.com>
2023-05-07 11:04:59 +0530

    Time: 302 ms (10.24%), Space: 75.2 MB (5.39%) - LeetHub

--- 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.cpp
+++ 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.cpp
@@ -2,55 +2,51 @@ class Solution {
 public:
     int minSubarray(vector<int>& nums, int p) {
         
-        long long sum=0;
         int n=nums.size();
-    
-        vector<long long> v;
+        
+        long long sum=0;
         for(int i=0;i<n;i++)
         {
-            sum+=nums[i];   
-            v.push_back(nums[i]);
+            sum+=nums[i];
         }
-    
         
         int rem=sum%p;
+        
         if(rem==0)
             return 0;
-        if(p>sum)
+        
+        if(sum<p)
             return -1;
+        
+        vector<long long> v;
+        v.push_back(nums[0]);
+        
+        for(int i=1;i<n;i++)
+        {
+            v.push_back(v.back()+nums[i]);
+        }
         
         map<int,int> mp;
         mp[0]=-1;
         int ans=1e9;
         
-        for(int i=1;i<n;i++)
-        {
-            v[i]+=v[i-1];
-        }
-        
         for(int i=0;i<n;i++)
         {
             int r=v[i]%p;
-            cout<<r<<endl;
             r=r-rem;
+          //  cout<<r<<endl;
             
             if(r<0)
-            {
                 r+=p;
-            }
             
             if(mp.find(r)!=mp.end())
             {
-                if(i==n-1 && r==0 && mp[0]==-1)
-                {
-                    
-                }
+               // cout<<i<<" "<<mp[r]<<endl;
+                if(i==n-1 && mp[r]==-1)
+                {}
                 else
-                {
-                    ans=min(ans,i-mp[r]);
-                }
+                ans=min(ans,i-mp[r]);        
             }
-            
             mp[v[i]%p]=i;
         }
         
