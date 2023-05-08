45c9bf21edf6398f890dfb01fc85b9849fb73014
mayank <46345959+mayank171@users.noreply.github.com>
2023-05-08 15:04:28 +0530

    Time: 871 ms (5.03%), Space: 19.6 MB (95.30%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -5,50 +5,46 @@ public:
         int n=heights.size();
         int m=heights[0].size();
         
-        priority_queue< pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq;
-        
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq;
         pq.push({0,{0,0}});
         
-        vector<int> vis(n*m,INT_MAX);
-        
+        vector<int> vis(n*m,1e9);
         vis[0]=0;
         
-        int ans=1e8;
+        int dx[4]={-1,0,1,0};
+        int dy[4]={0,1,0,-1};
+        
+        int ans=1e9;
         
         while(!pq.empty())
         {
-            int diff=pq.top().first;
+            int h=pq.top().first;
             int x=pq.top().second.first;
             int y=pq.top().second.second;
-         
+            cout<<h<<" "<<x<<","<<y<<endl;
             pq.pop();
             
             if(x==n-1 && y==m-1)
             {
-                ans=min(ans,diff);
-              //  cout<<"+"<<ans<<endl;
-                break;;
+                ans=min(ans,h);
+              //  vis[x*m+y]=0;
+                cout<<ans<<endl;
+                continue;
             }
             
-            int dx[4]={-1,0,1,0};
-            int dy[4]={0,1,0,-1};
-            
-            for(int ind=0;ind<4;ind++)
+            for(int i=0;i<4;i++)
             {
-                int ind_x=dx[ind]+x;
-                int ind_y=dy[ind]+y;
+                int ind_x=dx[i]+x;
+                int ind_y=dy[i]+y;
                 
-                
-                
-                if(ind_x>=0 && ind_x<n && ind_y>=0 && ind_y<m && vis[ind_x*m+ind_y]>max(diff,abs(heights[x][y]-heights[ind_x][ind_y]))) 
-                {
-                    vis[ind_x*m+ind_y]=max(diff,abs(heights[x][y]-heights[ind_x][ind_y]));
-                    pq.push({max(diff,abs(heights[x][y]-heights[ind_x][ind_y])),{ind_x,ind_y}});
+                if(ind_x>=0 && ind_x<n && ind_y>=0 && ind_y<m && vis[ind_x*m+ind_y]>max(h,abs(heights[ind_x][ind_y]-heights[x][y])))
+                {            
+                    vis[ind_x*m+ind_y]=max(h,abs(heights[ind_x][ind_y]-heights[x][y]));
+                    pq.push({max(h,abs(heights[ind_x][ind_y]-heights[x][y])),{ind_x,ind_y}});       
                 }
             }
         }
         
-        //cout<<endl;
         return ans;
         
     }
