4a087b23a51e3c5b3004d146c4b1891962ab3a0f
mayank <46345959+mayank171@users.noreply.github.com>
2023-05-30 19:53:49 +0530

    Time: 123 ms (41.58%), Space: 41.2 MB (70.22%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.cpp
+++ 0705-design-hashset/0705-design-hashset.cpp
@@ -1,37 +1,39 @@
-#include<bits/stdc++.h>
-
 class MyHashSet {
 public:
     
     vector<list<int>> v;
+    
     MyHashSet() {
         vector<list<int>> temp(100);
         v=temp;
     }
     
-    list<int>::iterator search(int key)
+    list<int> :: iterator search(int key)
     {
         int ind=key%100;
+        
         return find(v[ind].begin(),v[ind].end(),key);
     }
     
     void add(int key) {
         
+        if(contains(key))
+            return ;
         int ind=key%100;
-        if(search(key)!=v[ind].end())
-            return;
         v[ind].push_back(key);
     }
     
     void remove(int key) {
-         int ind=key%100;
-         if(contains(key))
-             v[ind].erase(search(key));
+        
+        if(!contains(key))
+            return ;
+        int ind=key%100;
+        v[ind].erase(search(key));
     }
     
     bool contains(int key) {
         
-         int ind=key%100;
+        int ind=key%100;
         if(search(key)!=v[ind].end())
             return true;
         return false;
