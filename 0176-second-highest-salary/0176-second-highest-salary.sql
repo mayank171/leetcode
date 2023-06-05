e324394047e62a2b5c236fae1bf0499d457f21ca
mayank <46345959+mayank171@users.noreply.github.com>
2023-06-05 19:33:23 +0530

    Time: 428 ms (63.88%), Space: 0B (100.00%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,10 +1,38 @@
-#select max(salary) as SecondHighestSalary from employee where salary < (select max(salary) from employee)  ---> will work even if second highest salary does not exist in table
+select max(salary) as SecondHighestSalary from Employee where salary< (select max(salary) from Employee);
 
-#select salary as SecondHighestSalary from employee order by salary desc limit 1,1  ---> use if second highest salary is guaranteed to exist in table ( this query works for nth salary also)
 
-# select distinct(e1.salary) as SecondHighestSalary from employee e1 where 1=
-# (
-#     select count(distinct(e2.salary)) from employee e2 where e1.salary<e2.salary
-# )   #----> this also cant work if second highest salary doent exist  ( this works for nth salary also )
 
-select max(salary) as SecondHighestSalary from Employee where salary< (select max(salary) from Employee); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
