195e04589a1f168c14a582b692b4f3a986de250f
mayank <46345959+mayank171@users.noreply.github.com>
2023-06-05 19:52:33 +0530

    Time: 1560 ms (22.17%), Space: 0B (100.00%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,4 +1,1 @@
-# select dname as Department, ename as Employee, esalary as Salary from
-# (select e.id as eid,e.name as ename,e.salary as esalary,e.departmentId as edid,d.id as did,d.name as dname from Employee e inner join Department d on e.departmentId=d.id) as E where (did,esalary) in (select departmentId,max(salary) from employee group by departmentId);
-select department.name as Department,E.name as Employee,E.salary as Salary from
-(select *,max(salary) over(partition by departmentId) as maxi from Employee) as E inner join department on E.departmentId=department.id where E.maxi=E.salary;
+select d.name as Department,e.name as Employee,e.salary as Salary from department d inner join employee e on e.departmentId=d.id where (departmentId,salary) in (select departmentId,max(salary) as Salary from Employee group by departmentId) ;
