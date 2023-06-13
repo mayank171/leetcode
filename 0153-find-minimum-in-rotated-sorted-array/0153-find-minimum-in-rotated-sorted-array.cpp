3bd29f77f5d62807588708bbabb193ca7a95c145
mayank <46345959+mayank171@users.noreply.github.com>
2023-06-13 20:02:55 +0530

    Time: 7 ms (19.98%), Space: 10.2 MB (55.53%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -3,45 +3,52 @@ public:
     int findMin(vector<int>& nums) {
         
         int n=nums.size();
-        
         int low=0;
         int high=n-1;
-        int key=-100000;
         int ans=1e9;
-        
         
         while(low<=high)
         {
             int mid=(low+high)/2;
-            //cout<<low<<" "<<mid<<" "<<high<<endl;
             
+          //  cout<<low<<" "<<high<<" "<<ans<<endl;
             
-            ans=min(ans,nums[mid]);
-            ans=min(ans,nums[low]);
-            ans=min(ans,nums[high]);
-            
-                
-            if(nums[mid]>=nums[low])
+            if(nums[mid]<=nums[low] && nums[mid]<=nums[high])
             {
-                if(nums[mid]>key && nums[low]<=key)
-                    high=mid-1;
-                else 
+                ans=min(ans,nums[mid]);
+                if(nums[low]<nums[high])
+                {
                     low=mid+1;
-            }
-            else if(nums[mid]<nums[high])
-            {
-                if(nums[mid]<key && nums[high]>=key)
-                    low=mid+1;
+                }
                 else
+                {
                     high=mid-1;
+                }
+            }
+            else if(nums[mid]>nums[low] && nums[mid]>nums[high])
+            {
+                if(nums[low]<nums[high])
+                {
+                    high=mid-1;
+                }
+                else
+                {
+                    low=mid+1;
+                }
+            }
+            else
+            {
+                if(nums[low]<nums[high])
+                {
+                    high=mid-1;
+                }
+                else
+                {
+                    low=mid+1;
+                }
             }
         }
         
-    //    ans=min(ans,nums[mid]);
-      //  ans=min(ans,nums[low]);
-       // ans=min(ans,nums[high]);
-            
         return ans;
-        
     }
 };
