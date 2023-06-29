5eea6e314d87d03e4b711dac1de549dacbf35dc1
mayank <46345959+mayank171@users.noreply.github.com>
2023-06-29 16:48:54 +0530

    Added solution - LeetHub

---
+++ Count total set bits - GFG/count-total-set-bits.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    int countSetBits(int n)
+    {
+        if(n<=0)
+           return 0;
+        
+        int p=log2(n);
+        
+        return p*(pow(2,p)/2)+(n-pow(2,p)+1)+countSetBits(n-pow(2,p));
+    }
+};
+
+
+//{ Driver Code Starts.
+
+// Driver code
+int main()
+{
+	 int t;
+	 cin>>t;// input testcases
+	 while(t--) //while testcases exist
+	 {
+	       int n;
+	       cin>>n; //input n
+	       Solution ob;
+	       cout << ob.countSetBits(n) << endl;// print the answer
+	  }
+	  return 0;
+}
+
+// } Driver Code Ends
