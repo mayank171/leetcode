f81dc3acaef82984c6a7a5f93f7734436b97cb83
mayank <46345959+mayank171@users.noreply.github.com>
2023-07-12 06:19:07 +0530

    Time: 259 ms (10.94%), Space: 60.1 MB (31.90%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
     
-    bool dfs(vector<int> graph[],int src,vector<int> &vis,vector<int> &dfsvis,set<int> &st)
+    bool dfs(vector<int> adj[],int n,int src,vector<int> &vis,vector<int> &dfsvis)
     {
-        cout<<src<<endl;
-       
         vis[src]=1;
         dfsvis[src]=1;
         
-        for(auto &it:graph[src])
+        for(auto &it:adj[src])
         {
-            
             if(vis[it]==0)
             {
-               
-                if(dfs(graph,it,vis,dfsvis,st))
-                {
-                    st.insert(src);
-                    return true;
-                }
-                
+               if(dfs(adj,n,it,vis,dfsvis))
+                   return true;
             }
             else if(dfsvis[it]==1)
             {
-                st.insert(src);
                 return true;
             }
-  
         }
         
         dfsvis[src]=0;
@@ -36,46 +26,39 @@ public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         
         int n=graph.size();
-        
         vector<int> adj[n];
-          set<int> st;
         
         for(int i=0;i<n;i++)
         {
-            int flag=0;
             for(int j=0;j<graph[i].size();j++)
             {
-                if(graph[i][j]==i)
-                    flag=1;
                 adj[i].push_back(graph[i][j]);
             }
-            if(flag==1)
-               st.insert(i);
         }
         
         vector<int> vis(n,0);
-      
         vector<int> dfsvis(n,0);
-        
         for(int i=0;i<n;i++)
         {
             if(vis[i]==0)
             {
-                dfs(adj,i,vis,dfsvis,st);
+                if(!dfs(adj,n,i,vis,dfsvis))
+                {
+                }
             }
         }
         
         vector<int> ans;
         for(int i=0;i<n;i++)
         {
-            if(st.find(i)!=st.end())
-                continue;
-            ans.push_back(i);
+            if(vis[i]==1 && dfsvis[i]==1)
+            {}
+            else
+                ans.push_back(i);
         }
         
         sort(ans.begin(),ans.end());
         
         return ans;
-        
     }
 };
