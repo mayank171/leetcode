f7bcdbf0306cf5fac5161cef2fa444f3c5ec5da8
mayank <46345959+mayank171@users.noreply.github.com>
2023-07-15 18:39:06 +0530

    Time: 303 ms (11.14%), Space: 57.5 MB (13.15%) - LeetHub

--- 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.cpp
+++ 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.cpp
@@ -1,10 +1,41 @@
 class Solution {
 public:
-    int longestSubsequence(vector<int>& arr, int difference) {
-        unordered_map<int,int> lengths;
-        int result=1;
-        for(int &i:arr)
-            result=max(result,lengths[i]=1+lengths[i-difference]); 
-        return result;
+    
+    int solve(vector<int> &arr,int n,int ind,int diff)
+    {
+        if(ind>=n)
+            return 0;
+
+        int i;
+        int ans=0;
+        int flag=0;
+        for(i=ind+1;i<n;i++)
+        {
+            if(arr[i]-arr[ind]==diff)
+            {
+                flag=1;
+                break;
+            }
+        }
+        
+        return max(1+solve(arr,n,i,diff),solve(arr,n,ind+1,diff));    
+    }
+    
+    int longestSubsequence(vector<int>& arr, int diff) {
+        
+        int n=arr.size();
+        //return 1+solve(arr,n,0,difference);
+        
+        map<int,int> mp;
+   
+        int ans=1;
+        
+        for(int i=0;i<n;i++)
+        {
+            mp[arr[i]]=1+mp[arr[i]-diff];
+            ans=max(ans,mp[arr[i]]);    
+        }
+        
+        return ans;
     }
 };
