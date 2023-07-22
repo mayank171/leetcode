3790b7abbf1fd6510a0ca5328194c188264e355c
mayank <46345959+mayank171@users.noreply.github.com>
2023-07-22 17:08:07 +0530

    Time: 53 ms (59.89%), Space: 16.4 MB (33.83%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
     
-        int dx[4]={-1,0,1,0};
-        int dy[4]={0,1,0,-1};
-        
-    int dfs(vector<vector<int>> &matrix,int n,int m,int i,int j,vector<vector<int>> &dp)
-    {
-        
+    int dx[4]={-1,0,1,0};
+    int dy[4]={0,1,0,-1};
+    
+    int solve(vector<vector<int>> &matrix,int n,int m,int i,int j,vector<vector<int>> &dp)
+    {   
         if(dp[i][j]!=-1)
             return dp[i][j];
-        //d++;
         
-        int flag=0;
         int maxi=0;
         for(int ind=0;ind<4;ind++)
         {
-            int ind_x=i+dx[ind];
-            int ind_y=j+dy[ind];
-                        
-            if(ind_x>=0 && ind_x<n && ind_y>=0 && ind_y<m && matrix[i][j]<matrix[ind_x][ind_y])
+            int ind_x=dx[ind]+i;
+            int ind_y=dy[ind]+j;
+            
+            if(ind_x>=0 && ind_x<n && ind_y>=0 && ind_y<m && matrix[ind_x][ind_y]>matrix[i][j])
             {
-                flag=1;
-                maxi=max(maxi,dfs(matrix,n,m,ind_x,ind_y,dp)+1);
+                maxi=max(maxi,1+solve(matrix,n,m,ind_x,ind_y,dp));
             }
-        }    
-        
-    //    vis[i*m+j]=0;
+        }
         
         return dp[i][j]=maxi;
     }
@@ -34,21 +28,17 @@ public:
         
         int n=matrix.size();
         int m=matrix[0].size();
+        vector<vector<int>> dp(n+10,vector<int>(m+10,-1));
         
-        vector<vector<int>> dp(300,vector<int> (300,-1));
-        int res=0;
+        int ans=0;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                int ans=0;
-                ans=dfs(matrix,n,m,i,j,dp);
-                
-                res=max(res,ans);
+                ans=max(ans,solve(matrix,n,m,i,j,dp));
             }
-            
         }
         
-        return res+1;
+        return ans+1;
     }
 };
