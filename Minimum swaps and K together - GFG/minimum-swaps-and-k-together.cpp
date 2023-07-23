614819a09c520dd45532ac9af98ba429c1b677a4
mayank <46345959+mayank171@users.noreply.github.com>
2023-07-23 17:36:53 +0530

    Added solution - LeetHub

--- Minimum swaps and K together - GFG/minimum-swaps-and-k-together.cpp
+++ Minimum swaps and K together - GFG/minimum-swaps-and-k-together.cpp
@@ -13,9 +13,7 @@ class Solution
 {
 public:
     int minSwap(int arr[], int n, int k) {
-        
-        int ans=0;
-        
+        // Complet the function
         int ct=0;
         for(int i=0;i<n;i++)
         {
@@ -23,27 +21,28 @@ public:
                ct++;
         }
         
-        int mini=1e9;
-        int ctr=0;
+        int c=0;
+        int ans=1e5;
         for(int i=0;i<ct;i++)
         {
             if(arr[i]>k)
-               ctr++;
+               c++;
         }
         
-        mini=min(mini,ctr);
+        ans=min(ans,c);
+        //cout<<ans<<endl;
         
         for(int i=ct;i<n;i++)
         {
             if(arr[i-ct]>k)
-               ctr--;
+               c--;
             if(arr[i]>k)
-               ctr++;
-               
-            mini=min(mini,ctr);
+               c++;
+            
+            ans=min(ans,c);
         }
         
-        return mini;
+        return ans;
     }
 };
 
