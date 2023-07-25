b333954b37789e788eb7065799320fca0abde240
mayank <46345959+mayank171@users.noreply.github.com>
2023-07-25 10:04:44 +0530

    Time: 7 ms (88.50%), Space: 9.6 MB (59.74%) - LeetHub

--- 0685-redundant-connection-ii/0685-redundant-connection-ii.cpp
+++ 0685-redundant-connection-ii/0685-redundant-connection-ii.cpp
@@ -21,15 +21,13 @@ class dsu
         return parent[u]=findParent(parent[u]);
     }
     
-    void findUnion(int u,int v)
+    void unionFind(int u,int v)
     {
         int pu=findParent(u);
         int pv=findParent(v);
         
         if(pu==pv)
-        {
             return ;
-        }
         else if(size[pu]>=size[pv])
         {
             size[pu]+=size[pv];
@@ -45,41 +43,30 @@ class dsu
 
 class Solution {
 public:
-    
     vector<int> findRedundantDirectedConnection(vector<vector<int>>& edges) {
         
-        int n=edges.size();
-        vector<int> adj[n+1];
+        int size=edges.size();
         
-        for(int i=0;i<n;i++)
-        {
-            int u=edges[i][0];
-            int v=edges[i][1];
-            adj[u].push_back(v);
-        }
-        
-        vector<int> vis1(n+1,-1);
-        vector<int> res;
         vector<int> blk1;
         vector<int> blk2;
         
-        for(int i=0;i<n;i++)
+        vector<int> parent(size+1,-1);
+        
+        for(int i=0;i<size;i++)
         {
             int u=edges[i][0];
             int v=edges[i][1];
-            if(vis1[v]==-1)
+            
+            if(parent[v]==-1)
             {
-                vis1[v]=i;
+                parent[v]=i;
             }
             else
             {
                 blk1.push_back(u);
                 blk1.push_back(v);
-                
-                int ind=vis1[v];
-                blk2.push_back(edges[ind][0]);
-                blk2.push_back(edges[ind][1]);
-                break;
+                blk2.push_back(edges[parent[v]][0]);
+                blk2.push_back(edges[parent[v]][1]);
             }
         }
         
@@ -91,61 +78,75 @@ public:
             cout<<it<<" ";
         cout<<endl;
         
-        if(blk1.size()>0 && blk2.size()>0)
+        
+        
+        if(blk1.size()==0 && blk2.size()==0)
         {
-            vector<int> ans;
-            dsu d(n+1);
-            for(int i=0;i<n;i++)
+            dsu d(size+1);
+            
+            for(int i=0;i<size;i++)
             {
                 int u=edges[i][0];
                 int v=edges[i][1];
-                
-                if(u==blk1[0] && v==blk1[1])
-                    continue;
-
                 if(d.findParent(u)!=d.findParent(v))
                 {
-                    d.findUnion(u,v);
+                    d.unionFind(u,v);
                 }
                 else
                 {
-                    ans.push_back(u);
-                    ans.push_back(v);
-                   // break;
+                    return {u,v};
                 }
             }
-
-            cout<<ans.size()<<endl;
-            
-            if(ans.size()==0)
-            {
-                return blk1;
-            }
-            
-            return blk2;
         }
         
-        
-        vector<int> ans;
-            dsu d(n+1);
-            for(int i=0;i<n;i++)
+        dsu d(size+1);            
+        for(int i=0;i<size;i++)
+        {
+            
+            int u=edges[i][0];
+            int v=edges[i][1];
+            
+            if(u==blk1[0] && v==blk1[1])
             {
-                int u=edges[i][0];
-                int v=edges[i][1];
-                
-
-                if(d.findParent(u)!=d.findParent(v))
-                {
-                    d.findUnion(u,v);
-                }
-                else
-                {
-                    ans.push_back(u);
-                    ans.push_back(v);
-                   // break;
-                }
+                continue;
             }
-
-        return ans;
+            
+            if(d.findParent(u)!=d.findParent(v))
+            {
+                d.unionFind(u,v);
+            }
+            else
+            {
+                return blk2;
+            }
+        }
+        
+        return blk1;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
