a5e9c6727f1c198e2dda5172d62b5ed3207def0c
mayank <46345959+mayank171@users.noreply.github.com>
2023-07-27 17:23:48 +0530

    Time: 1055 ms (65.14%), Space: 0B (100.00%) - LeetHub

--- 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -1,20 +1,27 @@
 
-# select 
-# substring(trans_date,1,7) as month,
-# country,
-# count(amount) over (partition by substring(trans_date,1,7),country) as trans_count,
-# count(state) over (partition by substring(trans_date,1,7),country) as approved_count, 
-# sum(amount) over (partition by substring(trans_date,1,7),country) as trans_total_amount,
-# sum(amount) over(partition by substring(trans_date,1,7),country) as approved_total_amount 
-# from transactions;
-
-
-select substring(trans_date,1,7) as month,
+with cte as
+(
+select 
+substring(trans_date,1,7) as month,
 country,
-count(amount) as trans_count,
-sum(state='approved') as approved_count,
-sum(amount) as trans_total_amount,
-sum(case when state='approved' then amount
-    else 0
-    end) as approved_total_amount
-from transactions group by substring(trans_date,1,7),country;
+count(amount) over (partition by substring(trans_date,1,7),country) as trans_count,
+sum(state='approved') over (partition by substring(trans_date,1,7),country) as approved_count, 
+sum(amount) over (partition by substring(trans_date,1,7),country) as trans_total_amount,
+sum(case when state="approved" then amount else 0 end) over(partition by substring(trans_date,1,7),country) as approved_total_amount 
+from transactions
+),
+
+cte2 as
+(select *,row_number() over (partition by month,country,trans_count,approved_count,trans_total_amount,approved_total_amount) as X from cte)
+
+select month,country,trans_count,approved_count,trans_total_amount,approved_total_amount from cte2 where X=1;
+
+# select substring(trans_date,1,7) as month,
+# country,
+# count(amount) as trans_count,
+# sum(state='approved') as approved_count,
+# sum(amount) as trans_total_amount,
+# sum(case when state='approved' then amount
+#     else 0
+#     end) as approved_total_amount
+# from transactions group by substring(trans_date,1,7),country;
