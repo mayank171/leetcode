39852775bb48eb9ca5fc40431ff3c9dc1208cd63
mayank <46345959+mayank171@users.noreply.github.com>
2023-07-30 18:49:25 +0530

    Time: 974 ms (85.60%), Space: 0B (100.00%) - LeetHub

--- 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -1,27 +1,9 @@
 
-with cte as
-(
 select 
 substring(trans_date,1,7) as month,
 country,
-count(amount) over (partition by substring(trans_date,1,7),country) as trans_count,
-sum(state='approved') over (partition by substring(trans_date,1,7),country) as approved_count, 
-sum(amount) over (partition by substring(trans_date,1,7),country) as trans_total_amount,
-sum(case when state="approved" then amount else 0 end) over(partition by substring(trans_date,1,7),country) as approved_total_amount 
-from transactions
-),
-
-cte2 as
-(select *,row_number() over (partition by month,country,trans_count,approved_count,trans_total_amount,approved_total_amount) as X from cte)
-
-select month,country,trans_count,approved_count,trans_total_amount,approved_total_amount from cte2 where X=1;
-
-# select substring(trans_date,1,7) as month,
-# country,
-# count(amount) as trans_count,
-# sum(state='approved') as approved_count,
-# sum(amount) as trans_total_amount,
-# sum(case when state='approved' then amount
-#     else 0
-#     end) as approved_total_amount
-# from transactions group by substring(trans_date,1,7),country;
+count(substring(trans_date,1,7)) as trans_count,
+sum(case when state="approved" then 1 else 0 end) as approved_count,
+sum(amount) as trans_total_amount,
+sum(case when state="approved" then amount else 0 end) as approved_total_amount
+from transactions group by substring(trans_date,1,7),country;
