f09ae183013affabd3e66ea8f8e5053c8f3a423b
mayank <46345959+mayank171@users.noreply.github.com>
2023-07-30 19:10:01 +0530

    Time: 1211 ms (80.38%), Space: 0B (100.00%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,6 +1,10 @@
 # Write your MySQL query statement below
 with cte1 as
-(select *,row_number() over (partition by customer_id order by order_date,customer_pref_delivery_date) as extra from delivery)
+(select * from delivery order by customer_id,order_date),
 
-select round(sum(case when order_date=customer_pref_delivery_date then 1
-           else 0 end)/count(*)*100,2) as immediate_percentage from cte1 where extra=1;
+cte2 as
+(select *,row_number() over (partition by customer_id order by order_date) as X from cte1)
+
+select 
+round(sum(case when order_date=customer_pref_delivery_date then 1 else 0 end)/count(*)*100,2) as immediate_percentage 
+from cte2 where X=1;
