cf77cc4a8f586068e96db81e3640f72c5b0331e9
mayank <46345959+mayank171@users.noreply.github.com>
2023-07-30 11:16:40 +0530

    Time: 201 ms (19.32%), Space: 55.9 MB (9.02%) - LeetHub

--- 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
@@ -3,36 +3,43 @@ public:
     
     int solve(vector<int> &v,int size,int n,long long mid)
     {
-        long long ans=0;
+        long long sum=0;
         for(int i=0;i<size;i++)
         {
-            if(v[i]<=mid)
-                ans+=v[i];
+            if(v[i]<mid)
+            {
+                sum+=v[i];
+            }
             else
-                ans+=mid;
+            {
+                sum+=mid;
+            }
         }
         
-        if(ans/(long)n>=mid)
+        sum=sum/(long)n;
+        
+        if(sum>=mid)
             return 1;
         return 0;
     }
     
-    
     long long maxRunTime(int n, vector<int>& batteries) {
         
         int size=batteries.size();
-        
         long long low=1;
         long long high=1e15;
         
-        long long ans=0;
         
+        long long ans=0;
         while(low<=high)
         {
             long long mid=low+(high-low)/2;
             
+         //   cout<<low<<" "<<high<<endl;
+            
             if(solve(batteries,size,n,mid))
             {
+         //       cout<<mid<<endl;
                 ans=max<long long>(ans,mid);
                 low=mid+1;
             }
