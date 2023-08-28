57ba878b96995f681c4cc21d6afa49c2b22e3d0a
mayank <46345959+mayank171@users.noreply.github.com>
2023-08-28 17:30:33 +0530

    Time: 3 ms (36.88%), Space: 6.7 MB (67.22%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -1,62 +1,82 @@
 class MyStack {
 public:
     
-    queue<int> q1;
-    queue<int> q2;
-    
+    // queue<int> q1;
+    // queue<int> q2;
+    queue<int> q;
     
     MyStack() {
         
     }
     
     void push(int x) {
-        q1.push(x);
+       // q1.push(x);
+        int size=q.size();
+        
+        q.push(x);
+        
+        while(size--)
+        {
+            int x=q.front();
+            q.pop();
+            q.push(x);
+        }
     }
     
     int pop() {
         
-        while(q1.size()>1)
-        {
-            q2.push(q1.front());
-            q1.pop();
-        }
+//         while(q1.size()>1)
+//         {
+//             q2.push(q1.front());
+//             q1.pop();
+//         }
         
-        int x=q1.front();
-        q1.pop();
+//         int x=q1.front();
+//         q1.pop();
         
-        while(!q2.empty())
-        {
-            q1.push(q2.front());
-            q2.pop();
-        }
+//         while(!q2.empty())
+//         {
+//             q1.push(q2.front());
+//             q2.pop();
+//         }
         
+//         return x;
+        
+        int x=q.front();
+        q.pop();
         return x;
     }
     
     int top() {
         
-        while(q1.size()>1)
-        {
-            q2.push(q1.front());
-            q1.pop();
-        }
+//         while(q1.size()>1)
+//         {
+//             q2.push(q1.front());
+//             q1.pop();
+//         }
         
-        int x=q1.front();
-        q1.pop();
-        q2.push(x);
+//         int x=q1.front();
+//         q1.pop();
+//         q2.push(x);
         
-        while(!q2.empty())
-        {
-            q1.push(q2.front());
-            q2.pop();
-        }
+//         while(!q2.empty())
+//         {
+//             q1.push(q2.front());
+//             q2.pop();
+//         }
         
-        return x;
+//         return x;
+        
+        return q.front();
     }
     
     bool empty() {
         
-        if(q1.size()==0)
+//         if(q1.size()==0)
+//             return true;
+//         return false;
+        
+        if(q.size()==0)
             return true;
         return false;
     }
