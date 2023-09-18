b5aeb2e960313cb9127be4f9fbc24a490781c2f6
mayank <46345959+mayank171@users.noreply.github.com>
2023-09-18 10:19:18 +0530

    Time: 8 ms (76.19%), Space: 10.8 MB (13.97%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1337-the-k-weakest-rows-in-a-matrix/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1337-the-k-weakest-rows-in-a-matrix/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -2,36 +2,67 @@ class Solution {
 public:
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
         
-        vector<pair<int,int>> vp;
+        //vector<pair<int,int>> vp;
         
         int n=mat.size();
         int m=mat[0].size();
         
+//         for(int i=0;i<n;i++)
+//         {
+//             int ct=0;
+//             for(int j=0;j<m;j++)
+//             {
+//                 if(mat[i][j]==1)
+//                     ct++;
+//                 else
+//                     break;
+//             }
+//             vp.push_back({ct,i});
+//         }
+        
+//         sort(vp.begin(),vp.end());
+        
+//         vector<int> res;
+        
+//         for(int i=0;i<n;i++)
+//         {
+//             res.push_back(vp[i].second);
+//             k--;
+//             if(k==0)
+//                 break;
+//         }
+        
+//         return res;
+
         for(int i=0;i<n;i++)
         {
-            int ct=0;
-            for(int j=0;j<m;j++)
-            {
-                if(mat[i][j]==1)
-                    ct++;
-                else
-                    break;
-            }
-            vp.push_back({ct,i});
+            mat[i].push_back(i);
         }
         
-        sort(vp.begin(),vp.end());
+        sort(mat.begin(),mat.end());
         
         vector<int> res;
-        
-        for(int i=0;i<n;i++)
+        for(int i=0;i<k;i++)
         {
-            res.push_back(vp[i].second);
-            k--;
-            if(k==0)
-                break;
+            res.push_back(mat[i].back());
         }
         
         return res;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
