14e1ed8e148eb8a8ba3ed10d37592618131c1a31
mayank <46345959+mayank171@users.noreply.github.com>
2023-09-30 17:06:27 +0530

    Added solution - LeetHub

--- Boolean Matrix - GFG/boolean-matrix.cpp
+++ Boolean Matrix - GFG/boolean-matrix.cpp
@@ -8,15 +8,13 @@ using namespace std;
 class Solution
 {   
     public:
-    //Function to modify the matrix such that if a matrix cell matrix[i][j]
-    //is 1 then all the cells in its ith row and jth column will become 1.
     void booleanMatrix(vector<vector<int> > &matrix)
     {
-        // code here 
         int n=matrix.size();
         int m=matrix[0].size();
-        vector<int> top(m,0);
-        vector<int> left(n,0);
+        
+        vector<int> v1(n,0);
+        vector<int> v2(m,0);
         
         for(int i=0;i<n;i++)
         {
@@ -24,15 +22,16 @@ class Solution
             {
                 if(matrix[i][j]==1)
                 {
-                    top[j]=1;
-                    left[i]=1;
+                    v1[i]=1;
+                    v2[j]=1;
                 }
             }
         }
         
+        
         for(int i=0;i<n;i++)
         {
-            if(left[i]==1)
+            if(v1[i]==1)
             {
                 for(int j=0;j<m;j++)
                 {
@@ -41,16 +40,17 @@ class Solution
             }
         }
         
-        for(int i=0;i<m;i++)
+        for(int j=0;j<m;j++)
         {
-            if(top[i]==1)
+            if(v2[j]==1)
             {
-                for(int j=0;j<n;j++)
+                for(int i=0;i<n;i++)
                 {
-                    matrix[j][i]=1;
+                    matrix[i][j]=1;
                 }
             }
         }
+        
         
         
     }
