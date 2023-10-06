8398c2335ee9d157e200a1138e84a295c5f14dc1
mayank <46345959+mayank171@users.noreply.github.com>
2023-10-06 15:51:56 +0530

    Time: 2 ms (49.96%), Space: 6.4 MB (29.03%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int integerBreak(int n) {
+        
+        if(n==2)
+        {
+            return 1;
+        }
+        else if(n==3)
+        {
+            return 2;
+        }
+        
+        if(n%3==0)
+        {
+            return pow(3,n/3);
+        }
+        else if(n%3==1)
+        {
+            return pow(3,n/3-1)*4;
+        }
+        else
+        {
+            return pow(3,n/3)*2;   
+        }
+    }
+};
