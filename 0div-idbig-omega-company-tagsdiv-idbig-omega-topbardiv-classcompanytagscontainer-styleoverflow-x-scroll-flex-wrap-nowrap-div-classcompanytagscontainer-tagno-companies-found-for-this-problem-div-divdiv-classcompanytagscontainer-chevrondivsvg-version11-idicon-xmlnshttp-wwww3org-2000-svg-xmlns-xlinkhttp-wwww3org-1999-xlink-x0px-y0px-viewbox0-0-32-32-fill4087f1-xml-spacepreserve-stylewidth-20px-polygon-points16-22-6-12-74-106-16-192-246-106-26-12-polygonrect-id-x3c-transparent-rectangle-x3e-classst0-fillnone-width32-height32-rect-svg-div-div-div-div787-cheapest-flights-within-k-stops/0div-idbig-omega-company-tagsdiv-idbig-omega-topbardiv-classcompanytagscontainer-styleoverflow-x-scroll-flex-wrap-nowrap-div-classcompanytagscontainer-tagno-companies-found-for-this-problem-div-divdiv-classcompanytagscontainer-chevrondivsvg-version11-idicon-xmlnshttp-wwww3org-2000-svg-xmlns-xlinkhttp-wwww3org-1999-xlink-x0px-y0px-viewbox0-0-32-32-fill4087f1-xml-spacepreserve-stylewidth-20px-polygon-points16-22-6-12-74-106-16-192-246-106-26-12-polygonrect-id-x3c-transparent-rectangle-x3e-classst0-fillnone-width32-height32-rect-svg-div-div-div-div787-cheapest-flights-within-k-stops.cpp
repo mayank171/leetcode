e913d403de70c3d9730c797775d7af00c0913016
mayank <46345959+mayank171@users.noreply.github.com>
2024-02-24 19:37:41 +0530

    Time: 15 ms (78.54%), Space: 16.6 MB (52.48%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div787-cheapest-flights-within-k-stops/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div787-cheapest-flights-within-k-stops.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div787-cheapest-flights-within-k-stops/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div787-cheapest-flights-within-k-stops.cpp
@@ -1,51 +1,56 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dest, int k) {
+        
+        vector<int> dist(n,1e5);
+        vector<int> vis(n,0);
+         priority_queue<pair<int,pair<int,int>>> pq;
         
         vector<pair<int,int>> adj[n];
-        int size=flights.size();
         
-        for(int i=0;i<size;i++)
+        for(int i=0;i<flights.size();i++)
         {
             int u=flights[i][0];
             int v=flights[i][1];
-            int cost=flights[i][2];
+            int d=flights[i][2];
             
-            adj[u].push_back({v,cost});
+            adj[u].push_back({v,d});
         }
         
         k++;
-        priority_queue<pair<int,pair<int,int>>> pq;
         pq.push({k,{0,src}});
-        vector<int> dist(n,1e9);
         dist[src]=0;
         
+        int ans=1e9;
         while(!pq.empty())
         {
-            int stops=pq.top().first;
-            int d=pq.top().second.first;
+            int kk=pq.top().first;
             int node=pq.top().second.second;
+            int dis=pq.top().second.first;
             pq.pop();
             
-            if(node==dst)
+            if(node==dest)
             {
-                continue;
-            }
+                ans=min(ans,dis);
+               
+            }    
             
             for(auto &it:adj[node])
             {
                 int adjnode=it.first;
-                int w=it.second;
-                if(stops>0 && dist[adjnode]>d+w)
+                int wt=it.second;
+                
+                if(dist[adjnode]>=wt+dis && kk-1>=0)
                 {
-                    dist[adjnode]=d+w;
-                    pq.push({stops-1,{dist[adjnode],adjnode}});
+                    dist[adjnode]=wt+dis;
+                    pq.push({kk-1,{dist[adjnode],adjnode}});
                 }
             }
         }
         
-        if(dist[dst]==1e9)
+        if(ans==1e9)
             return -1;
-        return dist[dst];
+        
+        return ans;
     }
 };
