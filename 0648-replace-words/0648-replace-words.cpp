77f843f5f8038b51605b3da39891e88e06cd80e4
mayank <46345959+mayank171@users.noreply.github.com>
2024-06-08 18:42:49 +0530

    Time: 74 ms (78.84%), Space: 78.5 MB (45.04%) - LeetHub

--- 0648-replace-words/0648-replace-words.cpp
+++ 0648-replace-words/0648-replace-words.cpp
@@ -1,39 +1,88 @@
+struct Node
+{
+    struct Node* freq[26];
+    int flag;
+};
+
+class Trie
+{
+    private:
+    Node* root;
+    public:
+    Trie()
+    {
+        root=new Node();
+    }
+    
+    void insert(string word)
+    {
+        Node* node=root;
+        int l=word.size();
+        for(int i=0;i<l;i++)
+        {
+            if(node->freq[word[i]-'a']==NULL)
+            {
+                node->freq[word[i]-'a']=new Node();
+            }
+            node=node->freq[word[i]-'a'];
+        }
+        
+        node->flag=1;
+    }
+    
+    string check(string word)
+    {
+        Node* node=root;
+        int l=word.size();
+        string res="";
+        for(int i=0;i<l;i++)
+        {
+            if(node->freq[word[i]-'a']!=NULL)
+            {
+                res+=word[i];
+                node=node->freq[word[i]-'a'];
+            }
+            else
+                break;
+            
+            if(node->flag==1)
+                break;
+        }
+        
+        if(node->flag==0)
+            return "";
+        return res;
+    }
+};
+
 class Solution {
 public:
     string replaceWords(vector<string>& dict, string sentence) {
         
+        Trie t;
+        for(int i=0;i<dict.size();i++)
+        {
+            t.insert(dict[i]);
+        }
         
         int n=sentence.size();
         string ans="";
-        set<string> st;
-        
-        for(int i=0;i<dict.size();i++)
-        {
-            st.insert(dict[i]);
-        }
-        
-        
         for(int i=0;i<n;i++)
         {
+            string str="";
             int j=i;
-            string s="";
-            int flag=0;
             while(j<n && sentence[j]!=' ')
             {
-                if(flag==0)
-                {
-                    s+=sentence[j];
-                    if(st.find(s)!=st.end())
-                    {
-                        ans+=s;
-                        flag=1;
-                    }    
-                }
+                str+=sentence[j];
                 j++;
             }
-            if(flag==0)
-                ans+=s;
-            ans+=' ';
+            
+            string res=t.check(str);
+            if(res.size()==0)
+                ans+=str;
+            else
+                ans+=res;
+            ans+=" ";
             i=j;
         }
         
