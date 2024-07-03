c9e3926fcad0dd6551661e6536d6ea9f7904faaa
mayank <46345959+mayank171@users.noreply.github.com>
2024-07-03 20:11:15 +0530

    Time: 316 ms (5.05%), Space: 38.7 MB (80.72%) - LeetHub

---
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int minDifference(vector<int>& nums) {
+        
+        int n=nums.size();
+        
+        if(n<=3)
+            return 0;
+        
+        sort(nums.begin(),nums.end());
+        
+        for(int i=0;i<n;i++)
+        {
+            cout<<nums[i]<<" ";
+        }
+        cout<<endl;
+        
+        int ans=nums[n-3-1]-nums[0];
+        cout<<ans<<endl;
+        
+        ans=min(ans,min(nums[n-3]-nums[1],nums[n-4]-nums[0]));
+        cout<<ans<<endl;
+        
+        for(int i=2;i<n-2;i++)
+        {
+            int x=nums[n-2]-nums[2];
+            int y=nums[n-3]-nums[1];
+            int z=nums[n-1]-nums[3];
+            int w=nums[n-4]-nums[0];
+            cout<<x<<" "<<y<<endl;
+            ans=min(ans,min(x,min(w,min(z,y))));
+        }
+        
+        ans=min(ans,min(nums[n-2]-nums[2],nums[n-1]-nums[3]));
+        
+        return ans;
+        
+//         0,1,5,10,14
+        
+        // - --
+        // -- -
+        // ---
+        //   ---
+        
+//         0,1,1,4,6,6,6
+        
+//         0,1,1,4,0,0,0
+//         0,1,1,4,1,1,1
+//         0,1,1,4,1,1,1   
+//         4,4,4,4,6,6,6    
+        
+//         0,1,2,2,6,6,6,
+//         6,6,6,2,6,6,6
+//         0,1,2,2,2,2,2
+    }
+};
